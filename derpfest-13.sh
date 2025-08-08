e9fe7f1cbbfd7e8c9180e4b6295b32aa553af28f
AbuRider <dinarysf276@gmail.com>
2025-08-08 16:51:28 +0700

    Update derpfest-13.sh

--- derpfest-13.sh
+++ derpfest-13.sh
@@ -5,7 +5,7 @@ rm -rf prebuilts/clang/host/linux-x86
 rm -rf prebuilts/rust
 
 # repo init rom
-repo init -u https://github.com/DerpFest-LOS/android_manifest.git -b 15.2 --git-lfs
+repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 13 --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
@@ -19,15 +19,15 @@ echo "==========================="
 # dontol kucai
 git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Derpfest-13 device/xiaomi/earth
 
-git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-22.2 vendor/xiaomi/earth
+git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-20 vendor/xiaomi/earth
 
 git clone https://github.com/LineageOS/android_kernel_xiaomi_earth.git -b lineage-22.2 kernel/xiaomi/earth
 
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-20 hardware/xiaomi
 
-git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-22.2 hardware/mediatek
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-20 hardware/mediatek
 
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-22.2 device/mediatek/sepolicy_vndr
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-20 device/mediatek/sepolicy_vndr
 
 git clone https://github.com/AbuRider/vendor_extra.git -b master vendor/lineage-priv/keys
 
