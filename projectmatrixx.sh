010cab08697f8c043e780426fe71abbd58e14ee0
AbuRider <dinarysf276@gmail.com>
2025-08-11 09:07:20 +0700

    Update projectmatrixx.sh

--- projectmatrixx.sh
+++ projectmatrixx.sh
@@ -17,16 +17,12 @@ echo "============="
 
 # dontol kucai
 rm -rf device/xiaomi/earth
-rm -rf kernel/xiaomi/earth
 rm -rf vendor/xiaomi/earth
-rm -rf hardware/mediatek
-rm -rf hardware/xiaomi
-rm -rf device/mediatek/sepolicy_vndr
 rm -rf vendor/lineage-priv/keys
 
 git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b matrixx device/xiaomi/earth
 
-git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-22.2 vendor/xiaomi/earth
+git clone https://github.com/AbuRider/proprietary_vendor_xiaomi_earth.git -b test/lineage-22.2 vendor/xiaomi/earth
 
 git clone https://github.com/LineageOS/android_kernel_xiaomi_earth.git -b lineage-22.2 kernel/xiaomi/earth
 
