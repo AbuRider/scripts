44c830e6d76372988fb3f431d8bcebcd228693c6
AbuRider <dinarysf276@gmail.com>
2025-08-15 01:11:15 +0700

    Update projectmatrixx.sh

--- projectmatrixx.sh
+++ projectmatrixx.sh
@@ -17,12 +17,16 @@ echo "============="
 
 # dontol kucai
 rm -rf device/xiaomi/earth
+rm -rf kernel/xiaomi/earth
+rm -rf hardware/xiaomi
+rm -rf hardware/mediatek
 rm -rf vendor/xiaomi/earth
+rm -rf device/mediatek/sepolicy_vndr
 rm -rf vendor/lineage-priv/keys
 
 git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b matrixx device/xiaomi/earth
 
-git clone https://github.com/AbuRider/proprietary_vendor_xiaomi_earth.git -b test/lineage-22.2 vendor/xiaomi/earth
+git clone https://github.com/AbuRider/proprietary_vendor_xiaomi_earth.git -b lineage-22.2 vendor/xiaomi/earth
 
 git clone https://github.com/LineageOS/android_kernel_xiaomi_earth.git -b lineage-22.2 kernel/xiaomi/earth
 
@@ -32,7 +36,7 @@ git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-
 
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-22.2 device/mediatek/sepolicy_vndr
 
-git clone https://github.com/AbuRider/vendor_extra.git -b master vendor/lineage-priv/keys
+git clone https://github.com/AbuRider/sign_keys_priv.git -b master vendor/lineage-priv/keys
 
 # Export
 export BUILD_USERNAME=yusup
