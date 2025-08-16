0544ed35249462b1ee8857fe3565b773b0ea6130
AbuRider <dinarysf276@gmail.com>
2025-08-16 17:22:44 +0700

    Update pixelexperiece.sh

--- pixelexperiece.sh
+++ pixelexperiece.sh
@@ -4,7 +4,7 @@
 rm -rf prebuilts/clang/host/linux-x86
 
 # repo init rom
-repo init -u https://github.com/PixelExperience-LEGACY-edition/manifest.git -b thirteen-plus --depth=1 --git-lfs
+repo init -u https://github.com/AbuRider/lmodroid_manifest.git -b fifteen-qpr2 --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
@@ -19,36 +19,32 @@ echo "============="
 rm -rf device/xiaomi/earth
 rm -rf kernel/xiaomi/earth
 rm -rf vendor/xiaomi/earth
-rm -rf hardware/xiaomiearthrf hardware/mediatek
+rm -rf hardware/xiaomi
+rm -rf hardware/mediatek
 rm -rf device/mediatek/sepolicy_vndr
 rm -rf vendor/priv-keys
 
-git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b pixelexp device/xiaomi/earth
+git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Lmodroid-15 device/xiaomi/earth
 
-git clone https://github.com/AbuRider/proprietary_vendor_xiaomi_earth.git -b lineage-20 vendor/xiaomi/earth
+git clone https://github.com/AbuRider/proprietary_vendor_xiaomi_earth.git -b lineage-22.2 vendor/xiaomi/earth
 
 git clone https://github.com/LineageOS/android_kernel_xiaomi_earth.git -b lineage-22.2 kernel/xiaomi/earth
 
-git clone https://github.com/AbuRider/android_hardware_xiaomi.git -b lineage-20 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi
 
-git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-20 hardware/mediatek
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-22.2 hardware/mediatek
 
-git clone https://github.com/AbuRider/android_device_mediatek_sepolicy_vndr.git -b lineage-20 device/mediatek/sepolicy_vndr
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-22.2 device/mediatek/sepolicy_vndr
 
-git clone https://github.com/AbuRider/sign_keys_priv.git -b pissel vendor/priv-keys
-
-# sepol 
-rm -rf system/sepolicy
-git clone --depth=1 https://github.com/AbuRider/system_sepolicy.git -b thirteen system/sepolicy
+git clone https://github.com/AbuRider/sign_keys_priv.git -b lmaodroid vendor/lmodroid-priv/keys 
 
 # Export
 export BUILD_USERNAME=yusup
 export BUILD_HOSTNAME=crave
 export TZ=Asia/Jakarta
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-echo "======= ExpExportne ======"
 
 # initiate build setup
 . build/envsetup.sh
-lunch aosp_earth-userdebug
+breakfast earth userdebug
 mka bacon -j$(nproc --all)
