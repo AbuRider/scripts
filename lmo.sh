c760914649b2e93f5c9e48c207f8f88d1bc60f17
AbuRider <dinarysf276@gmail.com>
2025-08-17 11:25:09 +0700

    Update lmo.sh

--- lmo.sh
+++ lmo.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Fix eror
+# fix eror
 rm -rf prebuilts/clang/host/linux-x86
 
 # repo init rom
-repo init -u https://github.com/AbuRider/lmodroid_manifest.git -b fifteen-qpr2 --git-lfs
+repo init -u https://github.com/AbuRider/lmodroid_manifest.git -b fourteen --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
@@ -15,25 +15,28 @@ echo "============="
 echo "Sync success"
 echo "============="
 
-# clone trees bg
-git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Lmodroid-15 device/xiaomi/earth
+# trees
+rm -rf device/xiaomi/earth
+rm -rf kernel/xiaomi/earth
+rm -rf vendor/xiaomi/earth
+rm -rf hardware/mediatek
+rm -rf hardware/xiaomi
+rm -rf device/mediatek/sepolicy_vndr
+rm -rf vendor/lmodroid-priv/keys
 
-git clone https://github.com/AbuRider/proprietary_vendor_xiaomi_earth.git -b lineage-22.2 vendor/xiaomi/earth
+git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b lmodroid device/xiaomi/earth
+
+git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-21 vendor/xiaomi/earth
 
 git clone https://github.com/LineageOS/android_kernel_xiaomi_earth.git -b lineage-22.2 kernel/xiaomi/earth
 
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi
 
-git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-22.2 hardware/mediatek
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-21 hardware/mediatek
 
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-22.2 device/mediatek/sepolicy_vndr
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-21 device/mediatek/sepolicy_vndr
 
-git clone https://github.com/AbuRider/sign_keys_priv.git -b lmaodroid vendor/lmodroid-priv/keys 
-
-# Fix eror
-rm -rf frameworks/base
-rm -rf packages/services/Car
-git clone --depth=1 https://github.com/AbuRider/platform_frameworks_base.git -b fifteen-qpr1 frameworks/base
+git clone https://github.com/AbuRider/vendor_extra.git -b lmodroid vendor/lmodroid-priv/keys
 
 # Export
 export BUILD_USERNAME=yusup
