8afa6260dbe21f60b15e6e1ebe28e48af210c02d
AbuRider <dinarysf276@gmail.com>
2025-08-23 19:34:53 +0700

    Create cr.sh

---
+++ cr.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# fix eror
+rm -rf prebuilts/clang/host/linux-x86
+
+# repo init rom
+repo init -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# sync
+/opt/crave/resync.sh || repo sync
+echo "============="
+echo "Sync success"
+echo "============="
+
+# trees
+git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b crdroid device/xiaomi/earth
+
+git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-21 vendor/xiaomi/earth
+
+git clone https://github.com/LineageOS/android_kernel_xiaomi_earth.git -b lineage-21 kernel/xiaomi/earth
+
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi
+
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-21 hardware/mediatek
+
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-21 device/mediatek/sepolicy_vndr
+
+git clone https://github.com/AbuRider/sign_keys_priv.git -b main vendor/lineage-priv/keys
+
+# Export
+export BUILD_USERNAME=dnryd
+export BUILD_HOSTNAME=pangokceria
+export TZ=Asia/Jakarta
+
+# initiate build setup
+. build/envsetup.sh
+brunch earth userdebug
