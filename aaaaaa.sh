55eff21f67b03fac0b4f4bf62cfa8bcd967ba207
AbuRider <dinarysf276@gmail.com>
2025-09-04 07:49:38 +0700

    Update aaaaaa.sh

--- aaaaaa.sh
+++ aaaaaa.sh
@@ -4,37 +4,29 @@
 rm -rf prebuilts/clang/host/linux-x86
 
 # repo init rom
-repo init --depth=1 -u https://github.com/AfterlifeOS/android_manifest.git -b 14 --git-lfs
+repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # sync
-/opt/crave/resync.sh || repo sync
+/opt/crave/resync.sh
 echo "============="
 echo "Sync success"
 echo "============="
 
-# Delete some stuff
-rm -rf packages/apps/Settings
-rm -rf packages/apps/AfterHome
-rm -rf frameworks/base
+# dontol kucai
+git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b InfinityX device/xiaomi/earth
 
-# Clone Trees
-git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Afterlife-14 device/xiaomi/earth
-
-git clone https://github.com/AbuRider/proprietary_vendor_xiaomi_earth.git -b lineage-21 vendor/xiaomi/earth
+git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-22.2 vendor/xiaomi/earth
 
 git clone https://github.com/LineageOS/android_kernel_xiaomi_earth.git -b lineage-22.2 kernel/xiaomi/earth
 
-git clone https://github.com/AbuRider/android_device_mediatek_sepolicy_vndr.git -b after-14 device/mediatek/sepolicy_vndr
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-22.2 hardware/mediatek
 
-git clone https://github.com/AbuRider/signing_keys.git -b afterlife vendor/after-priv/keys
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-22.2 device/mediatek/sepolicy_vndr
 
-# Add new dependencies for fix eror
-git clone https://github.com/AbuRider/afl_packages_apps_Settings.git -b 14 packages/apps/Settings --depth=1
-git clone https://github.com/AbuRider/packages_apps_AfterHome.git -b 14 packages/apps/AfterHome --depth=1 
-git clone https://github.com/AbuRider/frameworks_base_afl.git -b 14 frameworks/base --depth=1
+git clone https://github.com/AbuRider/signing_keys.git -b main vendor/lineage-priv/keys
 
 # Export
 export BUILD_USERNAME=dnryd
@@ -43,4 +35,5 @@ export TZ=Asia/Jakarta
 
 # initiate build setup
 . build/envsetup.sh
-goafterlife earth
+lunch infinity_earth-userdebug
+mka bacon -j$(nproc --all)
