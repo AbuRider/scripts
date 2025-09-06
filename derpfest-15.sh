412154cfd96514acee95ed956db319c8db4cafa1
AbuRider <dinarysf276@gmail.com>
2025-09-06 19:21:27 +0700

    Update derpfest-15.sh

--- derpfest-15.sh
+++ derpfest-15.sh
@@ -5,7 +5,7 @@ rm -rf prebuilts/clang/host/linux-x86
 rm -rf prebuilts/rust
 
 # repo init rom
-repo init -u https://github.com/DerpFest-LOS/android_manifest.git -b 15.2 --git-lfs
+repo init -u https://github.com/DerpFest-AOSP/android_manifest.git -b 16 --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
@@ -17,35 +17,27 @@ echo " sync repository success..."
 echo "==========================="
 
 # dontol kucai
-rm -rf device/xiaomi/earth
-rm -rf kernel/xiaomi/earth
-rm -rf vendor/xiaomi/earth
-rm -rf hardware/mediatek
-rm -rf hardware/xiaomi
-rm -rf device/mediatek/sepolicy_vndr
-rm -rf vendor/lineage-priv/keys
+git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Derpfest-16 device/xiaomi/earth
 
-git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Derpfest-15.2 device/xiaomi/earth
-
-git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-22.2 vendor/xiaomi/earth
+git clone https://github.com/AbuRider/proprietary_vendor_xiaomi_earth.git -b lineage-23 vendor/xiaomi/earth
 
 git clone https://github.com/LineageOS/android_kernel_xiaomi_earth.git -b lineage-22.2 kernel/xiaomi/earth
 
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.0 hardware/xiaomi
 
-git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-22.2 hardware/mediatek
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.0 hardware/mediatek
 
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-22.2 device/mediatek/sepolicy_vndr
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.0 device/mediatek/sepolicy_vndr
 
-git clone https://github.com/AbuRider/vendor_extra.git -b master vendor/lineage-priv/keys
+git clone https://github.com/AbuRider/signing_keys.git -b main vendor/lineage-priv/keys
 
 # Export
 export BUILD_USERNAME=yusup
-export BUILD_HOSTNAME=crave
+export BUILD_HOSTNAME=pangokceria
 export TZ=Asia/Jakarta
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 
 # initiate build setup
 . build/envsetup.sh
-lunch lineage_earth-bp1a-userdebug
+lunch lineage_earth-bp2a-userdebug
 mka derp -j$(nproc --all)
