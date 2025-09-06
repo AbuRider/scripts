ab33f13262844b9e9e0fd72895559e6b24bec34a
AbuRider <dinarysf276@gmail.com>
2025-09-06 19:47:45 +0700

    Update derpfest-15.sh

--- derpfest-15.sh
+++ derpfest-15.sh
@@ -21,11 +21,11 @@ git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Derpfes
 
 git clone https://github.com/AbuRider/proprietary_vendor_xiaomi_earth.git -b lineage-23 vendor/xiaomi/earth
 
-git clone https://github.com/LineageOS/android_kernel_xiaomi_earth.git -b lineage-22.2 kernel/xiaomi/earth
+git clone https://github.com/mt6768-dev/android_kernel_xiaomi_earth.git -b lineage-22.2 kernel/xiaomi/earth
 
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.0 hardware/xiaomi
 
-git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.0 hardware/mediatek
+git clone https://github.com/Aeron-Aeron/hardware_mediatek.git -b lineage-23.0 hardware/mediatek
 
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.0 device/mediatek/sepolicy_vndr
 
