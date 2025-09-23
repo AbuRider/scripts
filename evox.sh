ce7b07646713e214510667564e0f893b8689143a
AbuRider <dinarysf276@gmail.com>
2025-09-23 15:46:15 +0700

    Update evox.sh

--- evox.sh
+++ evox.sh
@@ -4,7 +4,7 @@
 rm -rf prebuilts/clang/host/linux-x86
 
 # repo init rom
-repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs
+repo init -u https://github.com/ProjectMatrixx/android.git -b 15.0 --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
@@ -15,17 +15,16 @@ echo "============="
 echo "Sync success"
 echo "============="
 
-# Remove
+# cloning trees
 rm -rf device/xiaomi/earth
 rm -rf kernel/xiaomi/earth
 rm -rf vendor/xiaomi/earth
-rm -rf hardware/xiaomi
 rm -rf hardware/mediatek
+rm -rf hardware/xiaomi
 rm -rf device/mediatek/sepolicy_vndr
-rm -rf vendor/evolution-priv/keys
+rm -rf vendor/lineage-priv/keys
 
-# trees
-git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b EvolutionX-15 device/xiaomi/earth
+git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Matrixx device/xiaomi/earth
 
 git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-22.2 vendor/xiaomi/earth
 
@@ -37,15 +36,15 @@ git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-
 
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-22.2 device/mediatek/sepolicy_vndr
 
-git clone https://github.com/AbuRider/signing_keys.git -b evox vendor/evolution-priv/keys
+git clone https://github.com/AbuRider/signing_keys.git -b main vendor/lineage-priv/keys
 
 # Export
-export BUILD_USERNAME=rora
+export BUILD_USERNAME=aisy
 export BUILD_HOSTNAME=umbrella
+export KBUILD_BUILD_USER=aisy
+export KBUILD_BUILD_HOST=pangokceria
 export TZ=Asia/Jakarta
 
 # initiate build setup
 . build/envsetup.sh
-lunch lineage_earth-bp1a-userdebug
-make installclean
-m evolution -j$(nproc --all)
+brunch earth userdebug
