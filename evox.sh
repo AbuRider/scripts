a924eaf0bb3ab409de29be3c887ac2123ade41ae
AbuRider <dinarysf276@gmail.com>
2025-09-25 19:50:28 +0700

    Update evox.sh

--- evox.sh
+++ evox.sh
@@ -1,41 +1,22 @@
 #!/bin/bash
-
-# fix eror
+# Remove Clang
 rm -rf prebuilts/clang/host/linux-x86
-
-# repo init rom
+# repo init
 repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs
-echo "=================="
-echo "Repo init success"
-echo "=================="
-
-# sync
 /opt/crave/resync.sh || repo sync
-echo "============="
-echo "Sync success"
-echo "============="
-
 # cloning trees
-git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b EvolutionX-15 device/xiaomi/earth
-
+git clone https://github.com/AbuRider/device_xiaomi_earth.git -b EvolutionX-15 device/xiaomi/earth
 git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-22.2 vendor/xiaomi/earth
-
 git clone https://github.com/mt6768-dev/android_kernel_xiaomi_earth.git -b lineage-22.2 kernel/xiaomi/earth
-
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi
-
 git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-22.2 hardware/mediatek
-
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-22.2 device/mediatek/sepolicy_vndr
-
 git clone https://github.com/AbuRider/signing_keys.git -b evox vendor/evolution-priv/keys
-
 # Export
-export BUILD_USERNAME=aisy
-export BUILD_HOSTNAME=umbrella
+export BUILD_USERNAME=dnryd
+export BUILD_HOSTNAME=pangokceria
 export TZ=Asia/Jakarta
-
-# initiate build setup
+# starts build setup !
 . build/envsetup.sh
 lunch lineage_earth-bp1a-userdebug
 m evolution -j$(nproc --aMatrixxixx
