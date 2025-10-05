5aa74d09d34fab0bdf0cf5ade19818a30428e96c
π <dinarysf276@gmail.com>
2025-10-06 06:46:01 +0700

    Update matrixx.sh

--- matrixx.sh
+++ matrixx.sh
@@ -4,7 +4,7 @@
 rm -rf prebuilts/clang/host/linux-x86
 
 # repo init rom
-repo init -u https://github.com/AbuRider/android_manifest_matrixx.git -b 14.0 --git-lfs
+repo init -u https://github.com/ProjectMatrixx/android.git -b 15.0 --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
@@ -16,23 +16,24 @@ echo "Sync success"
 echo "============="
 
 # trees
-git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Matrixx-14 device/xiaomi/earth
+git clone https://github.com/AbuRider/device_xiaomi_earth.git -b Matrixx device/xiaomi/earth
 
-git clone https://github.com/AbuRider/proprietary_vendor_xiaomi_earth.git -b lineage-21 vendor/xiaomi/earth
+git clone https://github.com/AbuRider/proprietary_vendor_xiaomi_earth.git -b lineage-22.2 vendor/xiaomi/earth
 
-git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b cip-dt2w kernel/xiaomi/earth
+git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b lineage-22.2 kernel/xiaomi/earth
 
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi
 
-git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-21 hardware/mediatek
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-22.2 hardware/mediatek
 
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-21 device/mediatek/sepolicy_vndr
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-22.2 device/mediatek/sepolicy_vndr
 
 git clone https://github.com/AbuRider/signing_keys.git -b main vendor/lineage-priv/keys
 
 # Export
 export BUILD_USERNAME=rora
 export BUILD_HOSTNAME=pangokceria
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Jakarta
 
 # initiate build setup
