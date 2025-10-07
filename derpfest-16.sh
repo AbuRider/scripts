c5df238b82fca3317dc3c642b817dfc7da7873ae
π <dinarysf276@gmail.com>
2025-10-07 09:42:28 +0700

    Update derpfest-16.sh

--- derpfest-16.sh
+++ derpfest-16.sh
@@ -8,8 +8,8 @@ repo init -u https://github.com/DerpFest-AOSP/android_manifest.git -b 16 --git-l
 /opt/crave/resync.sh || repo sync
 # Clone Trees
 git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Derpfest-16 device/xiaomi/earth
-git clone https://github.com/AbuRider/proprietary_vendor_xiaomi_earth.git -b lineage-22.2 vendor/xiaomi/earth
-git clone https://github.com/mt6768-dev/android_kernel_xiaomi_earth.git -b lineage-22.2 kernel/xiaomi/earth
+git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-23.0 vendor/xiaomi/earth
+git clone https://github.com/mt6768-dev/android_kernel_xiaomi_earth.git -b lineage-23.0 kernel/xiaomi/earth
 # Other Stuff's
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.0 hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.0 hardware/mediatek
