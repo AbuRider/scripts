f29b2ddd9318978dacd0ba8dd67b4d414ed9e7de
π <dinarysf276@gmail.com>
2025-10-09 14:30:40 +0700

    Update aaaaaa.sh

--- aaaaaa.sh
+++ aaaaaa.sh
@@ -1,39 +1,25 @@
 #!/bin/bash
-
-# fix eror
+# Remove Clang
 rm -rf prebuilts/clang/host/linux-x86
-
-# repo init rom
-repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault
-echo "=================="
-echo "Repo init success"
-echo "=================="
-
-# sync
-/opt/crave/resync.sh
-echo "============="
-echo "Sync success"
-echo "============="
-
-# dontol kucai
-git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b InfinityX device/xiaomi/earth
-
-git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-22.2 vendor/xiaomi/earth
-
-git clone https://github.com/LineageOS/android_kernel_xiaomi_earth.git -b lineage-22.2 kernel/xiaomi/earth
-
-git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-22.2 hardware/mediatek
-
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-22.2 device/mediatek/sepolicy_vndr
-
-git clone https://github.com/AbuRider/signing_keys.git -b InfinityX vendor/infinity-priv/keys
-
+# repo init
+repo init -u https://github.com/RisingOS-Revived/android -b sixteen --git-lfs
+# Sync 
+/opt/crave/resync.sh || repo sync
+# cloning trees
+git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b ngising device/xiaomi/earth
+git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-23.0 vendor/xiaomi/earth
+git clone https://github.com/mt6768-dev/android_kernel_xiaomi_earth.git -b lineage-23.0 kernel/xiaomi/earth
+# Other dependencies
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.0 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.0 hardware/mediatek
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.0 device/mediatek/sepolicy_vndr
+# Signing rom
+git clone https://github.com/AbuRider/keys.git -b main vendor/lineage-priv/keys
 # Export
-export BUILD_USERNAME=dnryd
+export BUILD_USERNAME=dnr
 export BUILD_HOSTNAME=pangokceria
-export TZ=Asia/Jakarta
-
-# initiate build setup
+export TZ="Asia/Jakarta"
+# starts build setup !
 . build/envsetup.sh
-lunch infinity_earth-userdebug
-mka bacon -j$(nproc --all)
+riseup earth userdebug
+rise b
