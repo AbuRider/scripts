48cd3f7cb96db006eba65be08a0f2b4fd3159afb
π <dinarysf276@gmail.com>
2025-10-12 09:47:36 +0700

    Update and rename aaaaaa.sh to ngisink.sh

--- ngisink.sh
+++ ngisink.sh
@@ -5,6 +5,14 @@ rm -rf prebuilts/clang/host/linux-x86
 repo init -u https://github.com/RisingOS-Revived/android -b sixteen --git-lfs
 # Sync 
 /opt/crave/resync.sh || repo sync
+# remove
+rm -rf device/xiaomi/earth
+rm -rf kernel/xiaomi/earth
+rm -rf vendor/xiaomi/earth
+rm -rf hardware/mediatek
+rm -rf hardware/xiaomi 
+rm -rf device/mediatek/sepolicy_vndr
+rm -rf vendor/lineage-priv/keys
 # cloning trees
 git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b ngising device/xiaomi/earth
 git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-23.0 vendor/xiaomi/earth
@@ -14,7 +22,6 @@ git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23
 git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.0 hardware/mediatek
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.0 device/mediatek/sepolicy_vndr
 # Signing rom
-rm -rf vendor/lineage-priv/keys
 git clone https://github.com/AbuRider/priv_keys.git -b main vendor/lineage-priv/keys
 # Export
 export BUILD_USERNAME=dnr
