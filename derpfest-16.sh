ab5229eb4322a9b2c2805ec0c825e89f4ee97d7a
π <dinarysf276@gmail.com>
2025-10-13 23:01:08 +0700

    Update derpfest-16.sh

--- derpfest-16.sh
+++ derpfest-16.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 # fix eror
 rm -rf prebuilts/clang/host/linux-x86
-rm -rf prebuilts/rust
 # repo init rom
-repo init -u https://github.com/DerpFest-AOSP/android_manifest.git -b 16 --git-lfs
+repo init -u https://github.com/AviumUI/android_manifests -b avium-15 --git-lfs
 # sync
 /opt/crave/resync.sh || repo sync
 # Clone Trees
-git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Derpfest-16 device/xiaomi/earth
-git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-23.0 vendor/xiaomi/earth
+git clone https://github.com/AbuRider/device_xiaomi_earth.git -b Avium device/xiaomi/earth
+git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-22.2 vendor/xiaomi/earth
 git clone https://github.com/mt6768-dev/android_kernel_xiaomi_earth.git -b lineage-23.0 kernel/xiaomi/earth
 # Other Stuff's
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.0 hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.0 hardware/mediatek
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.0 device/mediatek/sepolicy_vndr
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-22.2 hardware/mediatek
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-22.2 device/mediatek/sepolicy_vndr
 git clone https://github.com/AbuRider/priv_keys -b main vendor/lineage-priv/keys
 # Export
 export BUILD_USERNAME=dnryd
@@ -21,5 +20,5 @@ export BUILD_HOSTNAME=pangokceria
 export TZ=Asia/Jakarta
 # initiate build setup
 . build/envsetup.sh
-lunch lineage_earth-bp2a-user
-mka derp -j$(nproc --all)
+lunch lineage_earth-bp1a-userdebug
+mka bacon -j$(nproc --all)
