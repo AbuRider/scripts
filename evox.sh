28524ae02ee7a01e144aec494c748dd2bb5bd608
π <dinarysf276@gmail.com>
2025-10-16 10:06:14 +0700

    Refactor evox.sh for repo sync and cloning
    Updated repository initialization and cloning process for Evolution-X.

--- evox.sh
+++ evox.sh
@@ -3,10 +3,8 @@
 rm -rf prebuilts/clang/host/linux-x86
 # repo init
 repo init -u https://github.com/Evolution-X/manifest -b udc --git-lfs 
-
 # Sync repository
 /opt/crave/resync.sh || repo sync
-
 # Remove
 rm -rf device/xiaomi/earth
 rm -rf vendor/xiaomi/earth
@@ -15,24 +13,20 @@ rm -rf hardware/mediatek
 rm -rf hardware/xiaomi
 rm -rf device/mediatek/sepolicy_vndr
 rm -rf vendor/evolution-priv/keys
-
 # cloning trees
 git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b EvolutionX-14 device/xiaomi/earth
 git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-21 vendor/xiaomi/earth
 git clone https://github.com/mt6768-dev/android_kernel_xiaomi_earth.git -b lineage-23.0 kernel/xiaomi/earth
-
 # Other dependencies
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-21 hardware/mediatek
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-21 device/mediatek/sepolicy_vndr
 git clone https://github.com/AbuRider/priv_keys.git -b evox vendor/evolution-priv/keys
-
 # Export
 export BUILD_USERNAME=mugis
 export BUILD_HOSTNAME=rumiaprjkt
 export TZ="Asia/Jakarta"
 export RELAX_USES_LIBRARY_CHECK=true
-
 # starts build setup !
 . build/envsetup.sh
 lunch lineage_earth-userdebug
