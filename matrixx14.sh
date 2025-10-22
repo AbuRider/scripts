74085f23cb4cf149be12a3a00525d2b753fc6e3b
π <dinarysf276@gmail.com>
2025-10-22 20:39:48 +0700

    Update matrixx14.sh

--- matrixx14.sh
+++ matrixx14.sh
@@ -3,16 +3,12 @@
 rm -rf prebuilts/clang/host/linux-x86
 # repo init rom
 repo init -u https://github.com/AbuRider/android_manifest_matrixx.git -b 14.0 --git-lfs
-echo "=================="
-echo "Repo init success"
-echo "=================="
 # sync
 /opt/crave/resync.sh || repo sync
-echo "============="
-echo "Sync success"
-echo "============="
 # remove dependecies
 rm -rf device/xiaomi/earth kernel/xiaomi/earth vendor/xiaomi/earth
+rm -rf hardware/xiaomi hardware/mediatek device/mediatek/sepolicy_vndr
+rm -rf vendor/lineage-priv/keys
 # Cloning trees
 git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Matrixx-14 device/xiaomi/earth
 git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-21 vendor/xiaomi/earth
@@ -26,7 +22,7 @@ git clone https://github.com/AbuRider/priv_keys.git -b main vendor/lineage-priv/
 rm -rf frameworks/base 
 git clone --depth=1 https://github.com/AbuRider/android_frameworks_base_old.git -b 14.0 frameworks/base
 # Export
-export BUILD_USERNAME=mugis
+export BUILD_USERNAME=mona
 export BUILD_HOSTNAME=pangokceria
 export TZ=Asia/Jakarta
 # initiate build setup
