cd34cf87d629a59f76fb93a76e44d689db5a3e5c
π <dinarysf276@gmail.com>
2025-11-02 22:13:18 +0700

    Update and rename ngisink.sh to infinix.sh

--- infinix.sh
+++ infinix.sh
@@ -2,24 +2,26 @@
 # Remove Clang
 rm -rf prebuilts/clang/host/linux-x86
 # repo init
-repo init -u https://github.com/RisingOS-Revived/android -b sixteen --git-lfs
+repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
 # Sync 
 /opt/crave/resync.sh || repo sync
 # cloning trees
-git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b ngising device/xiaomi/earth
+git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b infinity device/xiaomi/earth
 git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-23.0 vendor/xiaomi/earth
-git clone https://github.com/mt6768-dev/android_kernel_xiaomi_earth.git -b lineage-23.0 kernel/xiaomi/earth
+git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b upstream kernel/xiaomi/earth
 # Other dependencies
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.0 hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.0 hardware/mediatek
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.0 device/mediatek/sepolicy_vndr
 # Signing rom
-git clone https://github.com/AbuRider/priv_keys.git -b main vendor/lineage-priv/keys
+git clone https://github.com/AbuRider/priv_keys.git -b infinity vendor/infinity-priv/keys
 # Export
-export BUILD_USERNAME=dnr
-export BUILD_HOSTNAME=rumiaprjkt
+export BUILD_USERNAME=mona
+export BUILD_HOSTNAME=rionenomoto 
+export KBUILD_BUILD_USER=mona
+export KBUILD_BUILD_HOST=crave
 export TZ="Asia/Jakarta"
 # starts build setup !
 . build/envsetup.sh
-riseup earth userdebug
-rise b
+lunch infinity_earth-userdebug
+mka bacon
