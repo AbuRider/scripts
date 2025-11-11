e3dd36515530720f24f297af3cf10c053c4cddf4
dnr <194088945+AbuRider@users.noreply.github.com>
2025-11-11 22:07:13 +0700

    Update lumi.sh

--- lumi.sh
+++ lumi.sh
@@ -5,14 +5,10 @@ rm -rf prebuilts/clang/host/linux-x86
 repo init -u https://github.com/LumineDroid/platform_manifest.git -b bynx --git-lfs
 # sync
 /opt/crave/resync.sh || repo sync
-# rm
-rm -rf device/xiaomi/earth kernel/xiaomi/earth vendor/xiaomi/earth
-rm -rf hardware/xiaomi hardware/mediatek device/mediatek/sepolicy_vndr
-rm -rf vendor/lineage-priv/keys
 # Clone Trees
-git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Lumine device/xiaomi/earth
+git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Lumine-16 device/xiaomi/earth
 git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-23.0 vendor/xiaomi/earth
-git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b upstream kernel/xiaomi/earth
+git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b 16 kernel/xiaomi/earth
 # Other Stuff's
 git clone https://github.com/AbuRider/android_hardware_xiaomi.git -b lineage-23.0 hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.0 hardware/mediatek
