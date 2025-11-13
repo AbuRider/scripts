0df1c64cd835d50faddec599d7bb88bcc4754394
dnr <194088945+AbuRider@users.noreply.github.com>
2025-11-14 04:53:57 +0700

    Update los-nx.sh

--- los-nx.sh
+++ los-nx.sh
@@ -1,51 +1,27 @@
 #!/bin/bash
-
 # fix eror
 rm -rf prebuilts/clang/host/linux-x86
 
 # repo init rom
-repo init -u https://github.com/los-nx/android.git -b lineage-22.2 --git-lfs
-echo "=================="
-echo "Repo init success"
-echo "=================="
-
-# sync
+repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs
 /opt/crave/resync.sh || repo sync
-echo "============="
-echo "Sync success"
-echo "============="
 
 # Cloning Trees
-rm -rf device/xiaomi/earth
-rm -rf vendor/xiaomi/earth
-rm -rf kernel/xiaomi/earth
-rm -rf hardware/mediatek
-rm -rf hardware/xiaomi
-rm -rf device/mediatek/sepolicy_vndr
-rm -rf vendor/lineage-priv/keys
+git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b crdroid device/xiaomi/earth
+git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-23.0 vendor/xiaomi/earth
+git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b 16 kernel/xiaomi/earth
 
-git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b lineage-22.2 device/xiaomi/earth
-
-git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-22.2 vendor/xiaomi/earth
-
-git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b cip-dt2w kernel/xiaomi/earth
-
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi
-
-git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-22.2 hardware/mediatek
-
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-22.2 device/mediatek/sepolicy_vndr
-
-git clone https://github.com/AbuRider/signing_keys.git -b main vendor/lineage-priv/keys
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.0 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.0 hardware/mediatek
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.0 device/mediatek/sepolicy_vndr
+git clone https://github.com/AbuRider/priv_keys.git -b main vendor/lineage-priv/keys
 
 # Export
-export BUILD_USERNAME=rora
-export BUILD_HOSTNAME=umbrella
-export KBUILD_BUILD_USER=aisy
-export KBUILD_BUILD_HOST=myrora
-export TZ=Asia/Jakarta
+export BUILD_USERNAME=reina
+export BUILD_HOSTNAME=zeroliner
+export TZ="Asia/Jakarta"
 
 # initiate build setup
 . build/envsetup.sh
-lunch lineage_earth-bp1a-userdebug
-make bacon -j$(nproc --all)
+lunch lineage_earth-bp2a-userdebug
+mka bacon
