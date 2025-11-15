772c7b6442161aadcfbb378bef3dd7a28fde3327
dnr <194088945+AbuRider@users.noreply.github.com>
2025-11-15 14:18:35 +0700

    Update lumi.sh

--- lumi.sh
+++ lumi.sh
@@ -20,7 +20,6 @@ git clone https://github.com/AbuRider/priv_keys.git -b main vendor/lineage-priv/
 # Export
 export BUILD_USERNAME=dnryd
 export BUILD_HOSTNAME=pangokceria
-export TZ="Asia/Jakarta"
 
 # initiate build setup
 . build/envsetup.sh
