c4ecffdbec0998d7d3da9425a9d7e9df477c3202
dnr <194088945+AbuRider@users.noreply.github.com>
2025-11-16 20:25:29 +0700

    Update matrixx14.sh

--- matrixx14.sh
+++ matrixx14.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # fix eror
+rm -rf external/chromium-webview/prebuilt/arm 
+rm -rf external/chromium-webview/prebuilt/arm64
+rm -rf external/chromium-webview/prebuilt/x86
+rm -rf external/chromium-webview/prebuilt/x86_64
 rm -rf prebuilts/clang/host/linux-x86
 # repo init rom
 repo init -u https://github.com/AbuRider/android_manifest_matrixx.git -b 14.0 --git-lfs
 # sync
 /opt/crave/resync.sh || repo sync
-# remove dependecies
-rm -rf device/xiaomi/earth kernel/xiaomi/earth vendor/xiaomi/earth
-rm -rf hardware/xiaomi hardware/mediatek device/mediatek/sepolicy_vndr
-rm -rf vendor/lineage-priv/keys
 # Cloning trees
 git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Matrixx-14 device/xiaomi/earth
 git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-21 vendor/xiaomi/earth
-git clone https://github.com/mt6768-dev/android_kernel_xiaomi_earth.git -b lineage-22.2 kernel/xiaomi/earth
+git clone https://github.com/AbuRider/android_kernel_bumi.git -b 4.19.306 kernel/xiaomi/earth
 # Other dependencies
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-21 hardware/mediatek
@@ -22,10 +22,9 @@ git clone https://github.com/AbuRider/priv_keys.git -b main vendor/lineage-priv/
 rm -rf frameworks/base 
 git clone --depth=1 https://github.com/AbuRider/android_frameworks_base_old.git -b 14.0 frameworks/base
 # Export
-export BUILD_USERNAME=mona
+export BUILD_USERNAME=asuka
 export BUILD_HOSTNAME=pangokceria
-export TZ=Asia/Jakarta
 # initiate build setup
 . build/envsetup.sh
-lunch lineage_earth-ap2a-user
+lunch lineage_earth-ap2a-userdebug
 mka bacon
