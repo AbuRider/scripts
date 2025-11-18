e5a926470c67f42cb6b9ff864c11f6ca79817bd9
dnr <194088945+AbuRider@users.noreply.github.com>
2025-11-18 21:19:19 +0700

    Update evox.sh

--- evox.sh
+++ evox.sh
@@ -3,33 +3,28 @@
 rm -rf prebuilts/clang/host/linux-x86
 
 # repo init
-repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs 
+repo init -u https://github.com/Evolution-X/manifest -b udc --git-lfs 
 
 # Sync repository
 /opt/crave/resync.sh || repo sync
 
-# Remove old stuff's
-rm -rf device/xiaomi/earth vendor/xiaomi/earth kernel/xiaomi/earth
-rm -rf hardware/mediatek hardware/xiaomi device/mediatek/sepolicy_vndr
-rm -rf vendor/evolution-priv/keys
-
 # cloning trees
-git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b EvolutionX-16 device/xiaomi/earth
-git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-23.0 vendor/xiaomi/earth
-git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b upstream kernel/xiaomi/earth
+git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b EvolutionX-14 device/xiaomi/earth
+git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-21 vendor/xiaomi/earth
+git clone https://github.com/AbuRider/android_kernel_bumi.git -b 4.19.306 kernel/xiaomi/earth
 
 # Other dependencies
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.0 hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.0 hardware/mediatek
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.0 device/mediatek/sepolicy_vndr
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-21 hardware/mediatek
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-21 device/mediatek/sepolicy_vndr
 git clone https://github.com/AbuRider/priv_keys.git -b evox vendor/evolution-priv/keys
 
 # Export
-export BUILD_USERNAME=mona
+export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=pangokceria
 export TZ="Asia/Jakarta"
 
 # starts build setup !
 . build/envsetup.sh
-lunch lineage_earth-bp2a-userdebug
+lunch lineage_earth-userdebug
 m evolution 
