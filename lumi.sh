9f04177c838df8c99ba954a05403bd3276f9fa8b
dnr <194088945+AbuRider@users.noreply.github.com>
2025-11-22 15:08:37 +0700

    ok

--- lumi.sh
+++ lumi.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+
 # Remove Clang
 rm -rf external/chromium-webview/prebuilt/arm 
 rm -rf external/chromium-webview/prebuilt/arm64
 rm -rf external/chromium-webview/prebuilt/x86
 rm -rf external/chromium-webview/prebuilt/x86_64
 rm -rf prebuilts/clang/host/linux-x86
+
 # repo init
 repo init -u https://github.com/ProjectMatrixx/android.git -b 15.0 --git-lfs
 # sync
 /opt/crave/resync.sh || repo sync
+
 # Clone trees
 git clone https://github.com/AbuRider/device_xiaomi_earth.git -b Matrixx device/xiaomi/earth
 git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-22.2 vendor/xiaomi/earth
@@ -18,6 +21,7 @@ git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22
 git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-22.2 hardware/mediatek
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-22.2 device/mediatek/sepolicy_vndr
 git clone https://github.com/AbuRider/priv_keys.git -b matrixx vendor/lineage-priv/keys
+
 # Oke
 rm -rf vendor/lineage
 rm -rf frameworks/base
@@ -25,10 +29,12 @@ rm -rf hardware/lineage/interfaces
 git clone https://github.com/AbuRider/android_vendor_matrixx.git -b 15.0 vendor/lineage
 git clone https://github.com/AbuRider/android_frameworks_base_A15_metrik.git -b 15.0 frameworks/base
 git clone https://github.com/LineageOS/android_hardware_lineage_interfaces.git -b lineage-22.2 hardware/lineage/interfaces
+
 # Export
 export BUILD_USERNAME=oumae
 export BUILD_HOSTNAME=pangokceria
 export TZ="Asia/Jakarta"
+
 # starts build setup !
 . build/envsetup.sh
 lunch lineage_earth-bp1a-userdebug
