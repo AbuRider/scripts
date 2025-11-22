eaef049c9312098ccd04e89db34c0cd2276ca84a
dnr <194088945+AbuRider@users.noreply.github.com>
2025-11-22 15:13:55 +0700

    Update lumi.sh

--- lumi.sh
+++ lumi.sh
@@ -15,7 +15,7 @@ repo init -u https://github.com/ProjectMatrixx/android.git -b 15.0 --git-lfs
 # Clone trees
 git clone https://github.com/AbuRider/device_xiaomi_earth.git -b Matrixx device/xiaomi/earth
 git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-22.2 vendor/xiaomi/earth
-git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b 16 kernel/xiaomi/earth
+git clone https://github.com/mt6768-dev/android_kernel_xiaomi_earth.git -b lineage-22.2 kernel/xiaomi/earth
 # other dependencies
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-22.2 hardware/mediatek
