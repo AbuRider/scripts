d6ca21ae213e021e968b2cd25002d3a4aa6ce8a0
dnr <aburider@proton.me>
2025-11-24 07:11:46 +0700

    oke

--- kkkk.sh
+++ kkkk.sh
@@ -13,7 +13,7 @@ repo init -u https://github.com/ProjectMatrixx/android.git -b 15.0 --git-lfs
 /opt/crave/resync.sh || repo sync
 
 # Clone trees
-git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b lineage-22.2 device/xiaomi/earth
+git clone https://github.com/AbuRider/device_xiaomi_earth.git -b Matrixx device/xiaomi/earth
 git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-22.2 vendor/xiaomi/earth
 git clone https://github.com/mt6768-dev/android_kernel_xiaomi_earth.git -b lineage-22.2 kernel/xiaomi/earth
 # other dependencies
@@ -26,9 +26,11 @@ git clone https://github.com/AbuRider/priv_keys.git -b matrixx vendor/lineage-pr
 rm -rf vendor/lineage
 rm -rf frameworks/base
 rm -rf hardware/lineage/interfaces
-git clone https://github.com/AbuRider/android_vendor_matrixx.git -b 15.0 vendor/lineage
-git clone https://github.com/AbuRider/android_frameworks_base_A15_metrik.git -b 15.0 frameworks/base
-git clone https://github.com/LineageOS/android_hardware_lineage_interfaces.git -b lineage-22.2 hardware/lineage/interfaces
+rm -rf system/core
+git clone --depth=1 https://github.com/AbuRider/android_vendor_matrixx.git -b 15.0 vendor/lineage
+git clone --depth=1 https://github.com/AbuRider/android_frameworks_base_A15_metrik.git -b 15.0 frameworks/base
+git clone --depth=1 https://github.com/LineageOS/android_hardware_lineage_interfaces.git -b lineage-22.2 hardware/lineage/interfaces
+git clone --depth=1 https://github.com/AbuRider/android_system_core.git -b 15.0 system/core
 
 # Export
 export BUILD_USERNAME=oumae
@@ -37,4 +39,5 @@ export TZ="Asia/Jakarta"
 
 # starts build setup !
 . build/envsetup.sh
-brunch earth 
+lunch lineage_earth-bp1a-userdebug
+mka bacon
