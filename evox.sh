c9fbdc6746c6f5bd21fd2c0a407a7845fe629c50
dnr <aburider@proton.me>
2025-11-25 09:20:01 +0700

    sepol suki

--- evox.sh
+++ evox.sh
@@ -9,15 +9,16 @@ repo init -u https://github.com/Evolution-X/manifest -b bq1 --git-lfs
 /opt/crave/resync.sh || repo sync
 
 # cloning trees
-rm -rf device/xiaomi/earth vendor/xiaomi/earth kernel/xiaomi/earth
+# rm -rf device/xiaomi/earth vendor/xiaomi/earth kernel/xiaomi/earth
 git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b EvolutionX-16.1 device/xiaomi/earth
 git clone https://github.com/AbuRider/proprietary_vendor_xiaomi_earth.git -b lineage-23.1 vendor/xiaomi/earth
 git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b 16 kernel/xiaomi/earth
 
 # Other dependencies
+rm -rf device/mediatek/sepolicy_vndr
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.0 hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.0 hardware/mediatek
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.0 device/mediatek/sepolicy_vndr
+git clone https://github.com/AbuRider/android_device_mediatek_sepolicy_vndr.git -b lineage-23.0 device/mediatek/sepolicy_vndr
 git clone https://github.com/AbuRider/priv_keys.git -b evox vendor/evolution-priv/keys
 git clone https://github.com/techyminati/android_vendor_mediatek_ims.git vendor/mediatek/ims
 
