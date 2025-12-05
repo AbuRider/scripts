8f8ef194a83b2a6dd7c639621cecf131f9784743
Oumae <aburider@proton.me>
2025-12-05 18:25:32 +0700

    Update matrixx14.sh

--- matrixx14.sh
+++ matrixx14.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
-# fix eror
 rm -rf prebuilts/clang/host/linux-x86
-# repo init rom
+
 repo init -u https://github.com/AbuRider/android_manifest_matrixx.git -b 14.0 --git-lfs
-# sync
 /opt/crave/resync.sh || repo sync
-# Cloning trees
+
 git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Matrixx-14 device/xiaomi/earth
 git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-21 vendor/xiaomi/earth
 git clone https://github.com/AbuRider/android_kernel_bumi.git -b 4.19.306 kernel/xiaomi/earth
-# Other dependencies
+
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-21 hardware/mediatek
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-21 device/mediatek/sepolicy_vndr
 git clone https://github.com/AbuRider/priv_keys.git -b main vendor/lineage-priv/keys
-# fix emror
+
 rm -rf frameworks/base 
 git clone --depth=1 https://github.com/AbuRider/android_frameworks_base_old.git -b 14.0 frameworks/base
-# Export
+
 export BUILD_USERNAME=oumae
 export BUILD_HOSTNAME=silvereuphonium
-# initiate build setup
+
 . build/envsetup.sh
 lunch lineage_earth-ap2a-userdebug
 mka bacon
