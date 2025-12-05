ce71c9b1017fde00b43759f092e1d699be6dc74f
Oumae <aburider@proton.me>
2025-12-05 18:24:49 +0700

    Update matrixx14.sh

--- matrixx14.sh
+++ matrixx14.sh
@@ -8,7 +8,7 @@ repo init -u https://github.com/AbuRider/android_manifest_matrixx.git -b 14.0 --
 # Cloning trees
 git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Matrixx-14 device/xiaomi/earth
 git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-21 vendor/xiaomi/earth
-git clone https://github.com/AbuRider/android_kernel_bumi.git -b 4.19.309 kernel/xiaomi/earth
+git clone https://github.com/AbuRider/android_kernel_bumi.git -b 4.19.306 kernel/xiaomi/earth
 # Other dependencies
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-21 hardware/mediatek
