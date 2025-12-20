184cf6ebedb738dead8b85bd617f4e146ff6f658
Oumae <aburider@proton.me>
2025-12-20 15:59:56 +0700

    Revise DerpFest 16 build script and repository links
    Updated repository URLs and branches for DerpFest 16 build setup.

--- derpfest-16.sh
+++ derpfest-16.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
-# fix eror
 rm -rf prebuilts/clang/host/linux-x86
-# repo init rom
-repo init -u https://github.com/AviumUI/android_manifests -b avium-15 --git-lfs
-# sync
+
+repo init -u https://github.com/DerpFest-AOSP/android_manifest.git -b 16 --git-lfs
 /opt/crave/resync.sh || repo sync
-# Clone Trees
-git clone https://github.com/AbuRider/device_xiaomi_earth.git -b Avium device/xiaomi/earth
-git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-22.2 vendor/xiaomi/earth
-git clone https://github.com/mt6768-dev/android_kernel_xiaomi_earth.git -b lineage-23.0 kernel/xiaomi/earth
-# Other Stuff's
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-22.2 hardware/mediatek
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-22.2 device/mediatek/sepolicy_vndr
-git clone https://github.com/AbuRider/priv_keys -b main vendor/lineage-priv/keys
-# Export
-export BUILD_USERNAME=dnryd
-export BUILD_HOSTNAME=pangokceria
-export TZ=Asia/Jakarta
-# initiate build setup
+
+git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Derpfest-16 device/xiaomi/earth
+git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-23.0 vendor/xiaomi/earth
+git clone https://github.com/SilverEuphonium/android_kernel_xiaomi_earth.git -b 16 kernel/xiaomi/earth
+
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.0 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.0 hardware/mediatek
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.0 device/mediatek/sepolicy_vndr
+git clone https://github.com/AbuRider/priv_keys.git -b main vendor/lineage-priv/keys
+
+export BUILD_USERNAME=oumae
+export BUILD_HOSTNAME=euphonium
+export TZ="Asia/Jakarta"
+
 . build/envsetup.sh
-lunch lineage_earth-bp1a-userdebug
-mka bacon -j$(nproc --all)
+lunch lineage_earth-bp2a-userdebug
+mka derp
