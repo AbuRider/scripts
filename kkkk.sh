ae45c1fc3b4d16c350a2ecb6e45d816564ff4729
Oumae <aburider@proton.me>
2025-12-20 07:42:33 +0700

    Update kkkk.sh

--- kkkk.sh
+++ kkkk.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 # Remove Clang
-rm -rf prebuilts/clang/host/linux-x86
+# rm -rf prebuilts/clang/host/linux-x86
 
-repo init -u https://github.com/ProjectMatrixx/android.git -b 15.0 --git-lfs
-/opt/crave/resync.sh || repo sync
+# repo init -u https://github.com/ProjectMatrixx/android.git -b 15.0 --git-lfs
+# /opt/crave/resync.sh || repo sync
 
-git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Matrixx-15 device/xiaomi/earth
-git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-22.2 vendor/xiaomi/earth
-git clone https://github.com/LineageOS/android_kernel_xiaomi_earth.git -b lineage-22.2 kernel/xiaomi/earth
+# git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Matrixx-15 device/xiaomi/earth
+# git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-22.2 vendor/xiaomi/earth
+# git clone https://github.com/LineageOS/android_kernel_xiaomi_earth.git -b lineage-22.2 kernel/xiaomi/earth
 
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-22.2 hardware/mediatek
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-22.2 device/mediatek/sepolicy_vndr
-git clone https://github.com/AbuRider/priv_keys.git -b matrixx vendor/lineage-priv/keys
+# git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi
+# git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-22.2 hardware/mediatek
+# git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-22.2 device/mediatek/sepolicy_vndr
+# git clone https://github.com/AbuRider/priv_keys.git -b matrixx vendor/lineage-priv/keys
 
 # Edited source
-rm -rf vendor/lineage vendor/addons frameworks/base hardware/lineage/interfaces system/core build/release
-git clone --depth=1 https://github.com/AbuRider/android_vendor_matrixx.git -b 15.0 vendor/lineage
-git clone --depth=1 https://github.com/AbuRider/android_vendor_addons.git -b 15.0 vendor/addons
+rm -rf frameworks/base # vendor/lineage vendor/addons hardware/lineage/interfaces system/core build/release
+# git clone --depth=1 https://github.com/AbuRider/android_vendor_matrixx.git -b 15.0 vendor/lineage
+# git clone --depth=1 https://github.com/AbuRider/android_vendor_addons.git -b 15.0 vendor/addons
 git clone --depth=1 https://github.com/AbuRider/android_frameworks_base_A15_metrik.git -b 15.0 frameworks/base
-git clone --depth=1 https://github.com/LineageOS/android_hardware_lineage_interfaces.git -b lineage-22.2 hardware/lineage/interfaces
-git clone --depth=1 https://github.com/AbuRider/android_system_core.git -b 15.0 system/core
-git clone --depth=1 https://github.com/AbuRider/build_release.git -b 15.0 build/release
+# git clone --depth=1 https://github.com/LineageOS/android_hardware_lineage_interfaces.git -b lineage-22.2 hardware/lineage/interfaces
+# git clone --depth=1 https://github.com/AbuRider/android_system_core.git -b 15.0 system/core
+# git clone --depth=1 https://github.com/AbuRider/build_release.git -b 15.0 build/release
 
 # Export
 export BUILD_USERNAME=oumae
