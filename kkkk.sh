f5ef55bb4988c8f0f3ae8d9d78bdd9787bce22c5
Oumae <aburider@proton.me>
2025-12-27 05:57:02 +0700

    Update kkkk.sh

--- kkkk.sh
+++ kkkk.sh
@@ -1,33 +1,25 @@
 #!/bin/bash
-# Remove Clang
-# rm -rf prebuilts/clang/host/linux-x86
+# fix eror
+rm -rf device/xiaomi/earth kernel/xiaomi/earth vendor/xiaomi/earth
+rm -rf prebuilts/clang/host/linux-x86
+rm -rf hardware/mediatek hardware/xiaomi device/mediatek/sepolicy_vndr vendor/lineage-priv/keys
 
-# repo init -u https://github.com/ProjectMatrixx/android.git -b 15.0 --git-lfs
-# /opt/crave/resync.sh || repo sync
+repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.1 --git-lfs
+/opt/crave/resync.sh || repo sync
 
-# git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Matrixx-15 device/xiaomi/earth
-# git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-22.2 vendor/xiaomi/earth
-# git clone https://github.com/LineageOS/android_kernel_xiaomi_earth.git -b lineage-22.2 kernel/xiaomi/earth
+git clone https://github.com/SilverEuphonium/android_device_xiaomi_earth.git -b Axion-16.1 device/xiaomi/earth
+git clone https://github.com/AbuRider/proprietary_vendor_xiaomi_earth.git -b lineage-23.1 vendor/xiaomi/earth
+git clone https://github.com/SilverEuphonium/android_kernel_xiaomi_earth.git -b 16 kernel/xiaomi/earth
 
-# git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi
-# git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-22.2 hardware/mediatek
-# git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-22.2 device/mediatek/sepolicy_vndr
-# git clone https://github.com/AbuRider/priv_keys.git -b matrixx vendor/lineage-priv/keys
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.0 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.0 hardware/mediatek
+git clone https://github.com/SilverEuphonium/android_device_mediatek_sepolicy_vndr.git -b lineage-23.0 device/mediatek/sepolicy_vndr
+git clone https://github.com/AbuRider/priv_keys.git -b main vendor/lineage-priv/keys
+git clone https://github.com/techyminati/android_vendor_mediatek_ims.git vendor/mediatek/ims
 
-# Edited source
-rm -rf frameworks/base # vendor/lineage vendor/addons hardware/lineage/interfaces system/core build/release
-# git clone --depth=1 https://github.com/AbuRider/android_vendor_matrixx.git -b 15.0 vendor/lineage
-# git clone --depth=1 https://github.com/AbuRider/android_vendor_addons.git -b 15.0 vendor/addons
-git clone --depth=1 https://github.com/AbuRider/android_frameworks_base_A15_metrik.git -b 15.0 frameworks/base
-# git clone --depth=1 https://github.com/LineageOS/android_hardware_lineage_interfaces.git -b lineage-22.2 hardware/lineage/interfaces
-# git clone --depth=1 https://github.com/AbuRider/android_system_core.git -b 15.0 system/core
-# git clone --depth=1 https://github.com/AbuRider/build_release.git -b 15.0 build/release
+export BUILD_USERNAME=kumiko
+export BUILD_HOSTNAME=altosaxophone
 
-# Export
-export BUILD_USERNAME=oumae
-export BUILD_HOSTNAME=pangokceria
-export TZ="Asia/Jakarta"
-
-# starts build setup !
 . build/envsetup.sh
-brunch earth
+axion earth userdebug vanilla
+ax -br
