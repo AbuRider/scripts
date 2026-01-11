c681b24e2007648992ca141674baf068931cf241
Kumiko <aburider@proton.me>
2026-01-11 14:33:54 +0700

    Update kkkk.sh

--- kkkk.sh
+++ kkkk.sh
@@ -12,7 +12,7 @@ git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b 16 kern
 
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
 git clone https://github.com/AbuRider/android_hardware_mediatek.git -b lineage-23.2 hardware/mediatek
-git clone https://github.com/AbuRider/android_device_mediatek_sepolicy_vndr.git -b lineage-23.0 device/mediatek/sepolicy_vndr
+git clone https://github.com/SilverEuphonium/android_device_mediatek_sepolicy_vndr.git -b lineage-23.2 device/mediatek/sepolicy_vndr
 git clone https://github.com/AbuRider/sign.git -b evok vendor/evolution-priv/keys
 git clone https://github.com/techyminati/android_vendor_mediatek_ims.git vendor/mediatek/ims
 
