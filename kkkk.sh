c6ac255333d2c1f05ff432262257a95c908a1027
Kumiko <aburider@proton.me>
2026-01-14 05:57:42 +0700

    Update kkkk.sh

--- kkkk.sh
+++ kkkk.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
-# rm -rf prebuilts/clang/host/linux-x86
+rm -rf prebuilts/clang/host/linux-x86
 
-# repo init -u https://github.com/PixelOS-AOSP/android_manifest.git -b sixteen-qpr1 --git-lfs
-# /opt/crave/resync.sh || repo sync
+repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.1 --git-lfs
+/opt/crave/resync.sh || repo sync
 
-# git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b PixelOS-16.1 device/xiaomi/earth
-# git clone https://github.com/AbuRider/proprietary_vendor_xiaomi_earth.git -b lineage-23.1 vendor/xiaomi/earth
-# git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b 16 kernel/xiaomi/earth
+git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Axion-16.1 device/xiaomi/earth
+git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-23.0 vendor/xiaomi/earth
+git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b 16 kernel/xiaomi/earth
 
-rm -rf hardware/xiaomi
-git clone https://github.com/AbuRider/android_hardware_xiaomi.git -b lineage-23.1 hardware/xiaomi
-# git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.1 hardware/mediatek
-# git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.1 device/mediatek/sepolicy_vndr
-# git clone https://github.com/AbuRider/sign.git -b keys vendor/lineage-priv/keys
-# git clone https://github.com/techyminati/android_vendor_mediatek_ims.git vendor/mediatek/ims
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.1 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.1 hardware/mediatek
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.1 device/mediatek/sepolicy_vndr
+git clone https://github.com/AbuRider/sign.git -b keys vendor/lineage-priv/keys
 
 export BUILD_USERNAME=ririsaa
 export BUILD_HOSTNAME=wind_orchestra
 
 . build/envsetup.sh
-breakfast earth userdebug
-make installclean
-m pixelos
+axion earth userdebug vanilla
+ax -br
