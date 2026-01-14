6dd16af1a468d2f462ef848dabcfcc380785b1ca
Kumiko <aburider@proton.me>
2026-01-14 15:39:34 +0700

    Update kkkk.sh

--- kkkk.sh
+++ kkkk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -rf prebuilts/clang/host/linux-x86
 
-repo init -u https://github.com/AICP/platform_manifest.git -b w16.1 --git-lfs
+repo init -u https://github.com/AbuRider/platform_manifest.git -b w16.1 --git-lfs
 /opt/crave/resync.sh || repo sync
 
 git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Aicp-16.1 device/xiaomi/earth
