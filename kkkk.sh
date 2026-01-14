49e84c6beae822f9db16463888402e20b35fd022
Kumiko <aburider@proton.me>
2026-01-14 13:52:48 +0700

    Update kkkk.sh

--- kkkk.sh
+++ kkkk.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 rm -rf prebuilts/clang/host/linux-x86
 
-repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs
+repo init -u https://github.com/AICP/platform_manifest.git -b w16.1 --git-lfs
 /opt/crave/resync.sh || repo sync
 
-git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Crdroid-16.1 device/xiaomi/earth
+git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Aicp-16.1 device/xiaomi/earth
 git clone https://github.com/AbuRider/proprietary_vendor_xiaomi_earth.git -b lineage-23.1 vendor/xiaomi/earth
 git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b 16 kernel/xiaomi/earth
 
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.1 hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.1 hardware/mediatek
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.1 device/mediatek/sepolicy_vndr
-git clone https://github.com/AbuRider/sign.git -b keys vendor/lineage-priv/keys
+git clone https://github.com/AbuRider/sign.git -b aicp vendor/aicp-priv/keys
 git clone https://github.com/MillenniumOSS/android_vendor_mediatek_ims.git -b sixteen vendor/mediatek/ims
 
 export BUILD_USERNAME=ririsaa
