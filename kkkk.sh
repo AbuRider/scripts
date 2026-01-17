0a78b491099b682a8b17614c512408823555fcc7
Kumiko <aburider@proton.me>
2026-01-17 10:45:05 +0700

    Update kkkk.sh

--- kkkk.sh
+++ kkkk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf prebuilts/clang/host/linux-x86
-rm -rf vendor/aicp-priv/keys device/xiaomi/earth kernel/xiaomi/earth vendor/xiaomi/earth
+rm -rf vendor/lineage-priv/keys device/xiaomi/earth kernel/xiaomi/earth vendor/xiaomi/earth
 rm -rf hardware/mediatek hardware/xiaomi device/mediatek/sepolicy_vndr
 
 repo init -u https://github.com/The-Clover-Project/manifest.git -b 16-qpr2 --git-lfs
@@ -13,7 +13,7 @@ git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b 16 kern
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.1 hardware/mediatek
 git clone https://github.com/AbuRider/android_device_mediatek_sepolicy_vndr.git -b clover device/mediatek/sepolicy_vndr
-git clone https://github.com/AbuRider/sign.git -b keys2 vendor/lineage-priv/keys
+git clone https://github.com/AbuRider/sign.git -b keys2 vendor/clover-priv/keys
 git clone https://github.com/techyminati/android_vendor_mediatek_ims.git vendor/mediatek/ims
 
 export BUILD_USERNAME=ririsaa
