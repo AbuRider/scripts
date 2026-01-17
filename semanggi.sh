6a682e5c8f4cde53396b4009200b56c67cff3ba2
Kumiko <aburider@proton.me>
2026-01-17 11:54:25 +0700

    Update semanggi.sh

--- semanggi.sh
+++ semanggi.sh
@@ -3,7 +3,7 @@
 # Export
 export BUILD_USERNAME=miyazono
 export BUILD_HOSTNAME=wind_orchestra
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES= true
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export ALLOW_MISSING_DEPENDENCIES=true
 
 # starts build setup !
