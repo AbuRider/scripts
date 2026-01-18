b4e28b92d390ad8ddc8f6756397563a17e5b1214
Kumiko <aburider@proton.me>
2026-01-18 08:36:24 +0700

    Update os.sh

--- os.sh
+++ os.sh
@@ -10,7 +10,7 @@ git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b EuOS-14
 git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-21 vendor/xiaomi/earth
 git clone https://github.com/AbuRider/android_kernel_bumi.git -b 4.19.306 kernel/xiaomi/earth
 
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi
+git clone https://github.com/AbuRider/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-21 hardware/mediatek
 git clone https://github.com/AbuRider/android_device_mediatek_sepolicy_vndr.git -b eunoia device/mediatek/sepolicy_vndr
 git clone https://github.com/AbuRider/sign.git -b evok vendor/os-priv/keys
