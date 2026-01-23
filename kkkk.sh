631c56fc48e82627fe68988505347b6237101e01
Kumiko <aburider@proton.me>
2026-01-23 15:19:12 +0700

    Add BUILD_BROKEN_MISSING_REQUIRED_MODULES variable

--- kkkk.sh
+++ kkkk.sh
@@ -23,6 +23,7 @@ git clone https://github.com/AbuRider/android_hardware_xiaomi.git -b lineage-23.
 export BUILD_USERNAME=megumi
 export BUILD_HOSTNAME=wind_orchestra
 export CUSTOM_MAINTAINER=Kumiko
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 
 . build/envsetup.sh
 breakfast earth userdebug
