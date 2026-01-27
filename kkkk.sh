3f2000d5f51c4878e141f5e267ef23dfbce583d2
Ougumi <aburider@proton.me>
2026-01-27 10:01:29 +0700

    Update kkkk.sh

--- kkkk.sh
+++ kkkk.sh
@@ -14,7 +14,7 @@ git clone https://github.com/AbuRider/android_hardware_xiaomi.git -b lineage-23.
 git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.1 hardware/mediatek
 git clone https://github.com/AbuRider/android_device_mediatek_sepolicy_vndr.git -b Lumine device/mediatek/sepolicy_vndr
 git clone https://github.com/AbuRider/sign.git -b lumine vendor/lumine-priv/keys
-git clone https://github.com/techyminati/android_vendor_mediatek_ims.git vendor/mediatek/ims
+git clone https://github.com/MillenniumOSS/android_vendor_mediatek_ims.git -b sixteen vendor/mediatek/ims
 
 export BUILD_USERNAME=megumi
 export BUILD_HOSTNAME=wind_orchestra
