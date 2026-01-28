084e6acd8aecd60e0bb2a766b0ddc82c1433b70a
Ougumi <aburider@proton.me>
2026-01-28 09:14:30 +0700

    Update crdruid.sh

--- crdruid.sh
+++ crdruid.sh
@@ -3,24 +3,24 @@ rm -rf prebuilts/clang/host/linux-x86
 rm -rf vendor/lineage-priv/keys device/xiaomi/earth kernel/xiaomi/earth vendor/xiaomi/earth
 rm -rf hardware/xiaomi hardware/xiaomi device/mediatek/sepolicy_vndr vendor/mediatek/ims
 
-repo init -u https://github.com/Mnzz-Prjkt/android_manifest.git -b sixteen-qpr1 --git-lfs
+repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle
 /opt/crave/resync.sh || repo sync
 
-git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b PixelOS-16.1 device/xiaomi/earth
+git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b lineage-23.2 device/xiaomi/earth
 git clone https://github.com/AbuRider/proprietary_vendor_xiaomi_earth.git -b lineage-23.1 vendor/xiaomi/earth
 git clone https://github.com/SilverEuphonium/android_kernel_xiaomi_earth.git -b 16.2 kernel/xiaomi/earth
 
-git clone https://github.com/AbuRider/android_hardware_xiaomi.git -b lineage-23.1 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.1 hardware/mediatek
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.1 device/mediatek/sepolicy_vndr
+git clone https://github.com/AbuRider/android_device_mediatek_sepolicy_vndr.git -b lineage-23.2 device/mediatek/sepolicy_vndr
 git clone https://github.com/AbuRider/sign.git -b keys2 vendor/lineage-priv/keys
-git clone https://github.com/techyminati/android_vendor_mediatek_ims.git vendor/mediatek/ims
+git clone https://github.com/MillenniumOSS/android_vendor_mediatek_ims.git -b sixteen vendor/mediatek/ims
 
 export BUILD_USERNAME=megumi
 export BUILD_HOSTNAME=wind_orchestra
+export TARGET_DISABLE_MATLOG=true
+export TARGET_SUPPORTS_BLUR=false
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 
 . build/envsetup.sh
-breakfast earth userdebug
-make installclean
-m pixelos
+brunch earth userdebug
