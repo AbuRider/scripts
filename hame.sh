399b500fb94609e99953535f1d71f0258ba3cff7
Ririsa <aburider@proton.me>
2026-02-09 00:55:38 +0700

    Update hame.sh

--- hame.sh
+++ hame.sh
@@ -8,7 +8,7 @@ git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Evoluti
 git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-23.2 vendor/xiaomi/earth
 git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b 16.2-staging kernel/xiaomi/earth
 
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
+git clone https://github.com/AbuRider/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.2 hardware/mediatek
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.2 device/mediatek/sepolicy_vndr
 git clone https://github.com/AbuRider/sign.git -b evok vendor/evolution-priv/keys
