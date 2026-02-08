616b9d74c87a9b9730911fe1e7dda9e93a17e667
Ririsa <aburider@proton.me>
2026-02-08 07:50:12 +0700

    Update kkkk.sh

--- kkkk.sh
+++ kkkk.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 rm -rf prebuilts/clang/host/linux-x86
-rm -rf vendor/lumine-priv/keys device/xiaomi/earth kernel/xiaomi/earth vendor/xiaomi/earth
+rm -rf vendor/clover-priv/keys device/xiaomi/earth kernel/xiaomi/earth vendor/xiaomi/earth
 rm -rf hardware/xiaomi hardware/xiaomi device/mediatek/sepolicy_vndr
 
-repo init -u https://github.com/Mnzz-Prjkt/android_manifest.git -b sixteen-qpr1 --git-lfs
+repo init -u https://github.com/Evolution-X/manifest -b bq2 --git-lfs
 /opt/crave/resync.sh || repo sync
 
 git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b PixelOS-16.1 device/xiaomi/earth
-git clone https://github.com/AbuRider/proprietary_vendor_xiaomi_earth.git -b lineage-23.1 vendor/xiaomi/earth
-git clone https://github.com/SilverEuphonium/android_kernel_xiaomi_earth.git -b 16.2 kernel/xiaomi/earth
+git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-23.2 vendor/xiaomi/earth
+git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b 16.2-staging kernel/xiaomi/earth
 
-git clone https://github.com/AbuRider/android_hardware_xiaomi.git -b lineage-23.1 hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.1 hardware/mediatek
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.1 device/mediatek/sepolicy_vndr
-git clone https://github.com/AbuRider/sign.git -b keys vendor/lineage-priv/keys
-git clone https://github.com/techyminati/android_vendor_mediatek_ims.git vendor/mediatek/ims
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.2 hardware/mediatek
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.2 device/mediatek/sepolicy_vndr
+git clone https://github.com/AbuRider/sign.git -b evok vendor/evolution-priv/keys
 
 export BUILD_USERNAME=megumi
 export BUILD_HOSTNAME=wind_orchestra
-export CUSTOM_MAINTAINER=Kumiko
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 
 . build/envsetup.sh
-breakfast earth userdebug
+lunch lineage_earth-bp4a-userdebug
 make installclean
-m pixelos
+m evolution
