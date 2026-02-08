97ebcf926b260f400dfee398bac80d9ee4361907
Ririsa <aburider@proton.me>
2026-02-09 00:46:33 +0700

    Update hame.sh

--- hame.sh
+++ hame.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 rm -rf prebuilts/clang/host/linux-x86
 
-repo init --depth=1 -u https://github.com/ProjectSakura/android.git -b 16-qpr0 --git-lfs
+repo init -u https://github.com/Evolution-X/manifest -b bq2 --git-lfs
 /opt/crave/resync.sh || repo sync
 
-git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Sakura-16 device/xiaomi/earth
-git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-23.0 vendor/xiaomi/earth
-git clone https://github.com/SilverEuphonium/android_kernel_xiaomi_earth.git -b 16 kernel/xiaomi/earth
+git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b EvolutionX-16.2 device/xiaomi/earth
+git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-23.2 vendor/xiaomi/earth
+git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b 16.2-staging kernel/xiaomi/earth
 
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.0 hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.0 hardware/mediatek
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.0 device/mediatek/sepolicy_vndr
-git clone https://github.com/AbuRider/priv_keys.git -b main vendor/lineage-priv/keys
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.2 hardware/mediatek
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.2 device/mediatek/sepolicy_vndr
+git clone https://github.com/AbuRider/sign.git -b evok vendor/evolution-priv/keys
 
-export BUILD_USERNAME=oumae
-export BUILD_HOSTNAME=euphonium
-export TZ="Asia/Jakarta"
+export BUILD_USERNAME=makinohara
+export BUILD_HOSTNAME=wind_orchestra
 
 . build/envsetup.sh
-lunch lineage_earth-bp2a-userdebug
-mka bacon
+lunch lineage_earth-bp4a-userdebug
+m evolution
