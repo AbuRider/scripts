b2d636a1fd2ed6f1a2e59da5d202868ac5efb43e
Ririsa <aburider@proton.me>
2026-02-10 15:39:29 +0700

    Update semanggi.sh

--- semanggi.sh
+++ semanggi.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
-rm -rf prebuilts/clang/host/linux-x86
+# rm -rf prebuilts/clang/host/linux-x86
+rm -rf device/xiaomi/earth packages/apps/EuiccPolicy
 
 repo init -u https://github.com/The-Clover-Project/manifest.git -b 16-qpr2 --git-lfs
 /opt/crave/resync.sh || repo sync
 
 git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Semanggi-16.2 device/xiaomi/earth
-git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-23.2 vendor/xiaomi/earth
-git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b 16.2-staging kernel/xiaomi/earth
+# git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-23.2 vendor/xiaomi/earth
+# git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b 16.2-staging kernel/xiaomi/earth
 
-git clone https://github.com/AbuRider/android_hardware_xiaomi.git -b Clover-16.2 hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.2 hardware/mediatek
-git clone https://github.com/AbuRider/android_device_mediatek_sepolicy_vndr.git -b Clover device/mediatek/sepolicy_vndr
-git clone https://github.com/AbuRider/sign.git -b keys2 vendor/lineage-priv/keys
+# git clone https://github.com/AbuRider/android_hardware_xiaomi.git -b Clover-16.2 hardware/xiaomi
+# git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.2 hardware/mediatek
+# git clone https://github.com/AbuRider/android_device_mediatek_sepolicy_vndr.git -b Clover device/mediatek/sepolicy_vndr
+# git clone https://github.com/AbuRider/sign.git -b keys2 vendor/lineage-priv/keys
 git clone https://github.com/LineageOS/android_packages_apps_EuiccPolicy.git -b lineage-23.2 packages/apps/EuiccPolicy
 
 export BUILD_USERNAME=megumi
 export BUILD_HOSTNAME=wind_orchestra
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 
 . build/envsetup.sh
 lunch clover_earth-bp4a-userdebug
+make installclean
 mka clover
