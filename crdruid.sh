58c1cb0e5256ff02c43e78f31740bd3b0aabb19b
Ririsa <aburider@proton.me>
2026-02-10 15:25:24 +0700

    Update crdruid.sh

--- crdruid.sh
+++ crdruid.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
-# rm -rf prebuilts/clang/host/linux-x86
-rm -rf device/xiaomi/earth # kernel/xiaomi/earth vendor/xiaomi/earth
-# rm -rf hardware/xiaomi hardware/xiaomi device/mediatek/sepolicy_vndr vendor/mediatek/ims
+rm -rf prebuilts/clang/host/linux-x86
 
-# repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle
-# /opt/crave/resync.sh || repo sync
+repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle
+/opt/crave/resync.sh || repo sync
 
 git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b lineage-23.2 device/xiaomi/earth
-# git clone https://github.com/AbuRider/proprietary_vendor_xiaomi_earth.git -b lineage-23.2 vendor/xiaomi/earth
-# git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b 16.2-staging kernel/xiaomi/earth
+git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-23.2 vendor/xiaomi/earth
+git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b 16.2-staging kernel/xiaomi/earth
 
-# git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
-# git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.2 hardware/mediatek
-# git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.2 device/mediatek/sepolicy_vndr
-# git clone https://github.com/AbuRider/sign.git -b keys2 vendor/lineage-priv/keys
-# git clone https://github.com/MillenniumOSS/android_vendor_mediatek_ims.git -b sixteen-qpr2 vendor/mediatek/ims
+git clone https://github.com/AbuRider/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.2 hardware/mediatek
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.2 device/mediatek/sepolicy_vndr
+git clone https://github.com/AbuRider/sign.git -b keys2 vendor/lineage-priv/keys
 
 export BUILD_USERNAME=megumi
 export BUILD_HOSTNAME=wind_orchestra
 export TARGET_ENABLE_BLUR=false
 export TARGET_DISABLE_MATLOG=true
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 
 . build/envsetup.sh
 lunch lineage_earth-bp4a-userdebug
