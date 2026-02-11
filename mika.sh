cf5c4bd6269fc1a0614b640d846488fb024cba7d
Ririsa <aburider@proton.me>
2026-02-11 20:44:12 +0700

    Update and rename mimik.sh to mika.sh

---
+++ mika.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# rm -rf prebuilts/clang/host/linux-x86
+
+# repo init -u https://github.com/AbuRider/manifest.git -b 16-qpr2 --git-lfs
+# /opt/crave/resync.sh || repo sync
+
+# git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Mica-16.2 device/xiaomi/earth
+# git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-23.2 vendor/xiaomi/earth
+# git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b 16.2-staging kernel/xiaomi/earth
+
+# git clone https://github.com/AbuRider/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
+# git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.2 hardware/mediatek
+# git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.2 device/mediatek/sepolicy_vndr
+# git clone https://github.com/AbuRider/sign.git -b mica vendor/mica-priv/keys
+# git clone https://github.com/LineageOS/android_packages_apps_FMRadio packages/apps/FMRadio
+
+rm -rf hardware/lineage/interfaces
+git clone https://github.com/Project-Mica/hardware_lineage_interfaces.git -b 16-qpr2 hardware/lineage/interfaces
+
+export BUILD_USERNAME=megumi
+export BUILD_HOSTNAME=wind_orchestra
+
+. build/envsetup.sh
+lunch mica_earth-bp4a-userdebug
+make installclean
+m mica-release
