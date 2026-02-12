466b20237bf03a5b5913680d7e1c438dcc25ebcd
Ririsa <aburider@proton.me>
2026-02-12 14:09:31 +0700

    Update crdruid.sh

--- crdruid.sh
+++ crdruid.sh
@@ -8,7 +8,7 @@ git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b lineage
 git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-23.2 vendor/xiaomi/earth
 git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b 16.2-staging kernel/xiaomi/earth
 
-git clone https://github.com/AbuRider/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.2 hardware/mediatek
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.2 device/mediatek/sepolicy_vndr
 git clone https://github.com/AbuRider/sign.git -b keys2 vendor/lineage-priv/keys
@@ -20,5 +20,4 @@ export TARGET_DISABLE_MATLOG=true
 
 . build/envsetup.sh
 lunch lineage_earth-bp4a-userdebug
-make installclean
 mka bacon
