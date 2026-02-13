8bde3bdd6e7827f56039549fb0fa4ee0e966b8dc
Ririsa <aburider@proton.me>
2026-02-13 16:38:03 +0700

    Update linek.sh

--- linek.sh
+++ linek.sh
@@ -2,23 +2,22 @@
 # Remove Clang
 rm -rf prebuilts/clang/host/linux-x86
 # repo init
-repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
+repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs
 # Sync repository
 /opt/crave/resync.sh || repo sync
 # cloning trees
-git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b lineage-21 device/xiaomi/earth
-git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-21 vendor/xiaomi/earth
-git clone https://github.com/mt6768-dev/android_kernel_xiaomi_earth.git -b lineage-22.2 kernel/xiaomi/earth
+git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b lineage-23.2 device/xiaomi/earth
+git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-23.2 vendor/xiaomi/earth
+git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b lineage-23.2 kernel/xiaomi/earth
 # Other dependencies
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-21 hardware/mediatek
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-21 device/mediatek/sepolicy_vndr
-git clone https://github.com/AbuRider/priv_keys.git -b main vendor/lineage-priv/keys
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.2 hardware/mediatek
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.2 device/mediatek/sepolicy_vndr
+git clone https://github.com/AbuRider/sign.git -b keys2 vendor/lineage-priv/keys
 # Export
-export BUILD_USERNAME=mugis
-export BUILD_HOSTNAME=pangokceria
-export TZ="Asia/Jakarta"
+export BUILD_USERNAME=kumiko
+export BUILD_HOSTNAME=wind_orchestra
 # starts build setup !
 . build/envsetup.sh
-lunch lineage_earth-ap2a-userdebug
+lunch lineage_earth-bp4a-userdebug
 mka bacon
