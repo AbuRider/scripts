612e9c5eacb104e2c673ff1b8b7457b319fc45c7
Ririsa <aburider@proton.me>
2026-02-15 21:02:11 +0700

    Update kkkk.sh

--- kkkk.sh
+++ kkkk.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+rm -rf prebuilts/clang/host/linux-x86
 rm -rf device/xiaomi/earth
+
+repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs
+/opt/crave/resync.sh || repo sync
+
 git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Lunaris-16.2 device/xiaomi/earth
 
 export BUILD_USERNAME=kumiko
