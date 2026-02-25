b756f10844d5c387454841fd62138e4754eab6ed
Ririsa <aburider@proton.me>
2026-02-25 19:46:13 +0700

    Update pos.sh

--- pos.sh
+++ pos.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 rm -rf prebuilts/clang/host/linux-x86
 
-repo init -u https://github.com/PixelOS-AOSP/android_manifest.git -b sixteen-qpr2 --git-lfs
+repo init -u https://github.com/AbuRider/pixelos_manifest.git -b sixteen-qpr2 --git-lfs
 /opt/crave/resync.sh || repo sync
 
+rm -rf device/xiaomi/earth
 git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b PixelOS-16.2 device/xiaomi/earth
-git clone https://github.com/AbuRider/proprietary_vendor_xiaomi_earth.git -b lineage-23.2 vendor/xiaomi/earth
-git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b 16.2-staging kernel/xiaomi/earth
 
-git clone https://github.com/AbuRider/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.2 hardware/mediatek
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.2 device/mediatek/sepolicy_vndr
-git clone https://github.com/AbuRider/sign.git -b keys2 vendor/lineage-priv/keys
-
-export BUILD_USERNAME=miyazono
+export BUILD_USERNAME=makinohara
 export BUILD_HOSTNAME=wind_orchestra
 
 . build/envsetup.sh
 breakfast earth userdebug
+make installclean
 m pixelos
