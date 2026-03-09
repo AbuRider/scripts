5c876f847ffa35d6293e26b01772fd4b9b65ca4e
makinohara <aburider@proton.me>
2026-03-10 03:15:57 +0700

    Update lunaris.sh

--- lunaris.sh
+++ lunaris.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+rm -rf prebuilts/clang/host/linux-x86
 
-rm -rf device/xiaomi/earth vendor/xiaomi/earth
+repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs
+/opt/crave/resync.sh || repo sync
+
 git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Lunaris-16.2 device/xiaomi/earth
 git clone https://github.com/AbuRider/vendor_xiaomi_earth.git -b 16.2 vendor/xiaomi/earth
+git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b 16.2-staging kernel/xiaomi/earth
+
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.2 hardware/mediatek
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.2 device/mediatek/sepolicy_vndr
+git clone https://github.com/AbuRider/sign.git -b keys2 vendor/lineage-priv/keys
 git clone https://github.com/MillenniumOSS/android_vendor_mediatek_ims.git -b sixteen-qpr2 vendor/mediatek/ims
 
 export BUILD_USERNAME=kumiko
