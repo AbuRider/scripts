dece2323099a561c295780a65294712dfb10131f
makinohara <aburider@proton.me>
2026-03-09 23:43:03 +0700

    Update lunaris.sh

--- lunaris.sh
+++ lunaris.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-rm -rf device/xiaomi/earth 
+
+rm -rf device/xiaomi/earth vendor/xiaomi/earth
 git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Lunaris-16.2 device/xiaomi/earth
+git clone https://github.com/AbuRider/vendor_xiaomi_earth.git -b 16.2 vendor/xiaomi/earth
+git clone https://github.com/MillenniumOSS/android_vendor_mediatek_ims.git -b sixteen-qpr2 vendor/mediatek/ims
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=clarinet_quartet
