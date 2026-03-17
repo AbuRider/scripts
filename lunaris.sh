ff140135fe7c042bbc4257eeffe9de07f6de7e24
Makinohara <aburider@proton.me>
2026-03-18 04:24:31 +0700

    Update lunaris.sh

--- lunaris.sh
+++ lunaris.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -rf prebuilts/clang/host/linux-x86
 
-repo init -u https://github.com/Lunaris-AOSP/android -b test --git-lfs
+repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs
 /opt/crave/resync.sh || repo sync
 
 git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Lunaris-16.2 device/xiaomi/earth
@@ -19,4 +19,5 @@ export BUILD_HOSTNAME=clarinet_quartet
 
 . build/envsetup.sh
 lunch lineage_earth-bp4a-userdebug
+make installclean
 m bacon
