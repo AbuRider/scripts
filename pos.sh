f6cf7a2a1ed8dc69f4a0315339258a2bf5793970
Makinohara <aburider@proton.me>
2026-03-28 08:02:44 +0700

    Update pos.sh

--- pos.sh
+++ pos.sh
@@ -5,14 +5,7 @@ repo init -u https://github.com/PixelOS-AOSP/android_manifest.git -b sixteen-qpr
 /opt/crave/resync.sh || repo sync
 
 git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b PixelOS-16.2 device/xiaomi/earth
-git clone https://github.com/AbuRider/vendor_xiaomi_earth.git -b 16.2 vendor/xiaomi/earth
-git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b 16.2-staging kernel/xiaomi/earth
-
-git clone https://github.com/AbuRider/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.2 hardware/mediatek
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.2 device/mediatek/sepolicy_vndr
 git clone https://github.com/AbuRider/sign.git -b keys2 vendor/lineage-priv/keys
-git clone https://github.com/MillenniumOSS/android_vendor_mediatek_ims.git -b sixteen-qpr2 vendor/mediatek/ims
 
 export BUILD_USERNAME=makinohara
 export BUILD_HOSTNAME=kitauji_quartet
