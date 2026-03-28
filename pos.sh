18130158bd9b39f2e3f0d8e9c4cbb163acda15d8
Makinohara <aburider@proton.me>
2026-03-28 16:35:08 +0700

    Update pos.sh

--- pos.sh
+++ pos.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 rm -rf prebuilts/clang/host/linux-x86
-rm -rf device/xiaomi/earth # kernel/xiaomi/earth vendor/xiaomi/earth
 
-# repo init -u https://github.com/PixelOS-AOSP/android_manifest.git -b sixteen-qpr2 --git-lfs
-# /opt/crave/resync.sh || repo sync
+repo init -u https://github.com/PixelOS-AOSP/android_manifest.git -b sixteen-qpr2 --git-lfs
+/opt/crave/resync.sh || repo sync
 
 git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b PixelOS-16.2 device/xiaomi/earth
 
