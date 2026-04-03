296040945a1f53e67db79106d32b6a14ca6b7020
Makinohara <aburider@proton.me>
2026-04-03 11:38:19 +0700

    Update lunaris.sh

--- lunaris.sh
+++ lunaris.sh
@@ -12,7 +12,7 @@ git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b 16.2-st
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.2 hardware/mediatek
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.2 device/mediatek/sepolicy_vndr
-git clone https://github.com/AbuRider/sign.git -b keys2 vendor/lineage-priv/keys
+git clone https://github.com/AbuRider/sign.git -b matrixx vendor/matrixx-priv/keys
 git clone https://github.com/MillenniumOSS/android_vendor_mediatek_ims.git -b sixteen-qpr2 vendor/mediatek/ims
 
 export BUILD_USERNAME=kumiko
