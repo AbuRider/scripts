61df029c2565fb36a24c0e50f4b71dd5cd9ba845
Makinohara <aburider@proton.me>
2026-04-03 15:32:57 +0700

    Update projectmatrixx.sh

--- projectmatrixx.sh
+++ projectmatrixx.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
-# Remove Clang
 rm -rf prebuilts/clang/host/linux-x86
 
-repo init -u https://github.com/ProjectMatrixx/android.git -b 16.0 --git-lfs
+repo init -u https://github.com/ProjectMatrixx/android -b 16.2 --git-lfs
 /opt/crave/resync.sh || repo sync
 
-# Clone trees
-git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Matrixx-15 device/xiaomi/earth
-git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-22.2 vendor/xiaomi/earth
-git clone https://github.com/LineageOS/android_kernel_xiaomi_earth.git -b lineage-22.2 kernel/xiaomi/earth
+rm -rf device/xiaomi/earth
+git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Matrixx-16.2 device/xiaomi/earth
 
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-22.2 hardware/mediatek
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-22.2 device/mediatek/sepolicy_vndr
-git clone https://github.com/AbuRider/priv_keys.git -b matrixx vendor/lineage-priv/keys
-
-export BUILD_USERNAME=oumae
-export BUILD_HOSTNAME=pangokceria
-export TZ="Asia/Jakarta"
+export BUILD_USERNAME=kumiko
+export BUILD_HOSTNAME=clarinet_quartet
+export PRODUCT_NO_CAMERA=false
 
 . build/envsetup.sh
-brunch earth
+lunch matrixx_earth-bp4a-userdebug
+make instaallclean
+m matrixx
