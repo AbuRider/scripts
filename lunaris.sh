402378135459d033eca3e83e9019de9afb7b6aac
Makinohara <aburider@proton.me>
2026-04-03 11:30:20 +0700

    Update lunaris.sh

--- lunaris.sh
+++ lunaris.sh
@@ -2,10 +2,10 @@
 rm -rf prebuilts/clang/host/linux-x86
 rm -rf device/xiaomi/earth
 
-repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs
+repo init -u https://github.com/ProjectMatrixx/android -b 16.2 --git-lfs
 /opt/crave/resync.sh || repo sync
 
-git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Lunaris-16.2 device/xiaomi/earth
+git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Matrixx-16.2 device/xiaomi/earth
 git clone https://github.com/AbuRider/vendor_xiaomi_earth.git -b 16.2 vendor/xiaomi/earth
 git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b 16.2-staging kernel/xiaomi/earth
 
@@ -19,5 +19,5 @@ export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=clarinet_quartet
 
 . build/envsetup.sh
-lunch lineage_earth-bp4a-userdebug
-m bacon
+lunch matrixx_earth-bp4a-userdebug
+m matrixx
