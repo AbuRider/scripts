3bcbc942dc6be553c49d2a6d1d671dee24031728
Makinohara <aburider@proton.me>
2026-04-04 08:54:58 +0700

    Update and rename projectmatrixx.sh to matrixx.sh

---
+++ matrixx.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# rm -rf prebuilts/clang/host/linux-x86
+
+# repo init -u https://github.com/ProjectMatrixx/android -b 16.2 --git-lfs
+# /opt/crave/resync.sh || repo sync
+
+rm -rf device/xiaomi/earth
+git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Matrixx-16.2 device/xiaomi/earth
+
+export BUILD_USERNAME=kumiko
+export BUILD_HOSTNAME=clarinet_quartet
+export PRODUCT_NO_CAMERA=false
+
+. build/envsetup.sh
+lunch matrixx_earth-bp4a-userdebug
+m matrixx
