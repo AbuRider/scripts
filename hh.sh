612422d587912ffb753cc1bfdd5a982c9fc10a33
Makinohara <aburider@proton.me>
2026-04-06 18:59:35 +0700

    Update hh.sh

--- hh.sh
+++ hh.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 rm -rf prebuilts/clang/host/linux-x86
 
-repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.2 --git-lfs
+repo init -u https://github.com/xdroid-oss/manifest.git -b z0 --git-lfs
 /opt/crave/resync.sh || repo sync
 
 git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Axion-2.5 device/xiaomi/earth
-git clone https://github.com/AbuRider/vendor_xiaomi_earth.git -b 16.2 vendor/xiaomi/earth
-git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b 16.2-staging kernel/xiaomi/earth
-
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.2 hardware/mediatek
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.2 device/mediatek/sepolicy_vndr
 git clone https://github.com/AbuRider/sign.git -b keys2 vendor/lineage-priv/keys
-git clone https://github.com/MillenniumOSS/android_vendor_mediatek_ims.git -b sixteen-qpr2 vendor/mediatek/ims
+git clone https://github.com/LineageOS/android_packages_apps_FMRadio.git -b lineage-23.2 packages/apps/FMRadio
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=clarinet_quartet
 
 . build/envsetup.sh
-axion earth userdebug vanilla
-ax -br
+breakfast earth userdebug
+m bacon
