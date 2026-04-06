d56d5fc09bca899f6972980ea0c7317c623258ca
Makinohara <aburider@proton.me>
2026-04-06 22:11:20 +0700

    Update hh.sh

--- hh.sh
+++ hh.sh
@@ -4,7 +4,7 @@ rm -rf prebuilts/clang/host/linux-x86
 repo init -u https://github.com/xdroid-oss/manifest.git -b z0 --git-lfs
 /opt/crave/resync.sh || repo sync
 
-git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Axion-2.5 device/xiaomi/earth
+git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b xd device/xiaomi/earth
 git clone https://github.com/AbuRider/sign.git -b keys2 vendor/lineage-priv/keys
 git clone https://github.com/LineageOS/android_packages_apps_FMRadio.git -b lineage-23.2 packages/apps/FMRadio
 
