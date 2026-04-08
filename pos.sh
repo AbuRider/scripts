82f15a465fbc6f95799729b8eb8e92a327351815
Makinohara <aburider@proton.me>
2026-04-08 21:31:32 +0700

    Update pos.sh

--- pos.sh
+++ pos.sh
@@ -7,7 +7,6 @@ repo init -u https://github.com/Kitauji-High-School/pixelos_manifest.git -b sixt
 /opt/crave/resync.sh || repo sync
 
 git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b PixelOS-16.2 device/xiaomi/earth
-git clone https://github.com/AbuRider/sign.git -b keys2 vendor/lineage-priv/keys
 
 export BUILD_USERNAME=shouko
 export BUILD_HOSTNAME=kitauji_quartet
