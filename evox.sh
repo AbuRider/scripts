50bec4ba2ae3337664f34d10d284d1b4ec997136
Makinohara <aburider@proton.me>
2026-04-16 05:40:14 +0700

    Update evox.sh

--- evox.sh
+++ evox.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 rm -rf prebuilts/clang/host/linux-x86 
-rm -rf device/xiaomi/earth vendor/xiaomi/earth kernel/xiaomi/earth vendor/mediatek/ims
-rm -rf hardware/xiaomi hardware/mediatek device/mediatek/sepolicy_vndr
 
 repo init -u https://github.com/Evolution-X/manifest -b bq2 --git-lfs
 /opt/crave/resync.sh || repo sync
