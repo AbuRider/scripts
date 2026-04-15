80ec28521f9000973d7f5510fa927a5a118c014b
Makinohara <aburider@proton.me>
2026-04-16 05:35:30 +0700

    Update evox.sh

--- evox.sh
+++ evox.sh
@@ -3,14 +3,14 @@ rm -rf prebuilts/clang/host/linux-x86
 rm -rf device/xiaomi/earth vendor/xiaomi/earth kernel/xiaomi/earth vendor/mediatek/ims
 rm -rf hardware/xiaomi hardware/mediatek device/mediatek/sepolicy_vndr
 
-repo init -u https://github.com/Kitauji-High-School/pixelos_manifest.git -b sixteen-qpr2 --git-lfs
+repo init -u https://github.com/Evolution-X/manifest -b bq2 --git-lfs
 /opt/crave/resync.sh || repo sync
 
-git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b PixelOS-16.2 device/xiaomi/earth
+git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b EvolutionX-16.2 device/xiaomi/earth
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=kitauji_quartet
 
 . build/envsetup.sh
-breakfast earth userdebug
-m pixelos
+lunch lineage_earth-bp4a-userdebug
+m evolution
