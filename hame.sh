d0a98bf66634a37b30f177b9834c361bcdab3fe4
Makinohara <aburider@proton.me>
2026-04-28 14:13:05 +0700

    Update hame.sh

--- hame.sh
+++ hame.sh
@@ -5,7 +5,7 @@ repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.2 --git-lfs
 /opt/crave/resync.sh || repo sync
 
 git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Axion-16.2 device/xiaomi/earth
-git clone https://github.com/Kitauji-High-School/proprietary_vendor_xiaomi_earth.git -b lineage-23.2 vendor/xiaomi/earth
+git clone https://github.com/Kitauji-High-School/vendor_xiaomi_earth.git -b 16.2 vendor/xiaomi/earth
 git clone https://github.com/Kitauji-High-School/android_kernel_xiaomi_earth.git -b 16.2-staging kernel/xiaomi/earth
 
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
