87733a5f7dfe1c68280e74845dada5fa81c8e507
Makinohara <aburider@proton.me>
2026-04-28 15:18:49 +0700

    Update hame.sh

--- hame.sh
+++ hame.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-rm -rf prebuilts/clang/host/linux-x86
+# rm -rf prebuilts/clang/host/linux-x86
+rm -rf device/xiaomi/earth kernel/xiaomi/earth vendor/xiaomi/earth
+rm -rf hardware/mediatek hardware/xiaomi device/mediatek/sepolicy_vndr
 
-repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.2 --git-lfs
-/opt/crave/resync.sh || repo sync
+# repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.2 --git-lfs
+# /opt/crave/resync.sh || repo sync
 
 git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Axion-16.2 device/xiaomi/earth
 git clone https://github.com/Kitauji-High-School/vendor_xiaomi_earth.git -b 16.2 vendor/xiaomi/earth
@@ -12,6 +14,7 @@ git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23
 git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.2 hardware/mediatek
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.2 device/mediatek/sepolicy_vndr
 git clone https://github.com/AbuRider/sign.git -b keys2 vendor/lineage-priv/keys
+git clone https://github.com/MillenniumOSS/android_vendor_mediatek_ims.git -b sixteen-qpr2 vendor/mediatek/ims
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=kitauji_quartet
