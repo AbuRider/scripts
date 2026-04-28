e722235a73708885c24237a428c87be669a67afc
Makinohara <aburider@proton.me>
2026-04-28 11:48:35 +0700

    Update hh.sh

--- hh.sh
+++ hh.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # rm -rf prebuilts/clang/host/linux-x86
 
-# repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle
-# /opt/crave/resync.sh || repo sync
+repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle
+/opt/crave/resync.sh || repo sync
 
 rm -rf device/xiaomi/earth
 git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b lineage-23.2 device/xiaomi/earth
@@ -13,4 +13,6 @@ export TARGET_SUPPORTS_BLUR=false
 export TARGET_DISABLE_MATLOG=true
 
 . build/envsetup.sh
-brunch earth userdebug
+lunch lineage_earth-bp4a-userdebug
+make installclean
+mka bacon
