6a58920715b24706ad367028a7e5ab05235559a5
Makinohara <aburider@proton.me>
2026-05-01 14:54:07 +0700

    Update evox.sh

--- evox.sh
+++ evox.sh
@@ -8,9 +8,9 @@ git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git
 git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-22.2 vendor/xiaomi/earth
 git clone https://github.com/LineageOS/android_kernel_xiaomi_earth.git -b lineage-22.2 kernel/xiaomi/earth
 
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.2 hardware/mediatek
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.2 device/mediatek/sepolicy_vndr
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-22.2 hardware/mediatek
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-22.2 device/mediatek/sepolicy_vndr
 git clone https://github.com/AbuRider/sign.git -b keys2 vendor/lineage-priv/keys
 
 export BUILD_USERNAME=kumiko
