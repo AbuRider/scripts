9149896c9e943ab811aec5516d1681ca5ff073df
Makinohara <aburider@proton.me>
2026-05-02 15:21:11 +0700

    Update lunaris.sh

--- lunaris.sh
+++ lunaris.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -rf device/xiaomi/earth
 
-repo init -u https://github.com/Lunaris-AOSP/android.git -b 16.2 --git-lfs
+repo init -u https://github.com/Lunaris-AOSP/android.git -b test --git-lfs
 /opt/crave/resync.sh || repo sync
 
 git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Lunaris-16.2 device/xiaomi/earth
