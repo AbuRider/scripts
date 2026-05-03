809d2036d156c1be6e2bb509d182ad1df0b0d417
Makinohara <aburider@proton.me>
2026-05-03 08:44:06 +0700

    Update evox.sh

--- evox.sh
+++ evox.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 rm -rf prebuilts/clang/host/linux-x86
 
-repo init -u https://github.com/Lunaris-AOSP/android.git -b 16.2 --git-lfs
+repo init -u https://github.com/Evolution-X/manifest -b bq2 --git-lfs
 /opt/crave/resync.sh || repo sync
 
-git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Lunaris-16.2 device/xiaomi/earth
+git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b EvolutionX-16.2 device/xiaomi/earth
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=clarinet_quartet
+export TARGET_SUPPORTS_GOOGLE_RECORDER=false
+export TARGET_INCLUDE_STOCK_ARCORE=false
 
 . build/envsetup.sh
-lunch lineage_earth-bp4a-user
-m bacon
+lunch lineage_earth-bp4a-userdebug
+m evolution
