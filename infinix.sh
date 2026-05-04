fb0651062e9e6ba9aba69f8efa08db4f3ed349da
Makinohara <aburider@proton.me>
2026-05-04 15:55:36 +0700

    Update infinix.sh

--- infinix.sh
+++ infinix.sh
@@ -4,7 +4,7 @@ rm -rf prebuilts/clang/host/linux-x86
 repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
 /opt/crave/resync.sh || repo sync
 
-git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Infinity-16.2 device/xiaomi/earth
+git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Infinity-16.2 device/xiaomi/earth
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=kitauji_quartet
