fefb8b4fbd05a294a84165fbaac50c03d0f5af67
Makinohara <aburider@proton.me>
2026-05-04 12:40:26 +0700

    Update evox.sh

--- evox.sh
+++ evox.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-rm -rf prebuilts/clang/host/linux-x86
 
 repo init -u https://github.com/Evolution-X/manifest -b bq2 --git-lfs
 /opt/crave/resync.sh || repo sync
 
+rm -rf device/xiaomi/earth
 git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b EvolutionX-16.2 device/xiaomi/earth
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=clarinet_quartet
-export TARGET_SUPPORTS_GOOGLE_RECORDER=false
-export TARGET_INCLUDE_STOCK_ARCORE=false
 
 . build/envsetup.sh
 lunch lineage_earth-bp4a-userdebug
+make installclean
 m evolution
