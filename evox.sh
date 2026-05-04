2f998a1fcdaae26498e6b09982d4105979236b1b
Makinohara <aburider@proton.me>
2026-05-04 12:40:44 +0700

    Update evox.sh

--- evox.sh
+++ evox.sh
@@ -7,7 +7,7 @@ rm -rf device/xiaomi/earth
 git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b EvolutionX-16.2 device/xiaomi/earth
 
 export BUILD_USERNAME=kumiko
-export BUILD_HOSTNAME=clarinet_quartet
+export BUILD_HOSTNAME=kitauji_quartet
 
 . build/envsetup.sh
 lunch lineage_earth-bp4a-userdebug
