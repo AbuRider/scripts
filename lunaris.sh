8b4fb42a3f927e44a7c2e3f5ec341d21d80c2934
Makinohara <aburider@proton.me>
2026-05-06 05:19:12 +0700

    Set up Lunaris AOSP environment for Xiaomi Earth
    Initialize repo and sync with Lunaris AOSP, clone device repository.

--- lunaris.sh
+++ lunaris.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-rm -rf device/xiaomi/earth
+repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs
+/opt/crave/resync.sh || repo sync
+
 git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Lunaris-16.2 device/xiaomi/earth
 
 export BUILD_USERNAME=kumiko
