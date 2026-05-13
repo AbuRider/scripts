5225adf8d2e99aaaec184e29907f15831dff575b
Makinohara <aburider@proton.me>
2026-05-13 19:31:05 +0700

    Update hame.sh

--- hame.sh
+++ hame.sh
@@ -7,6 +7,10 @@ repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs
 
 git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Lunaris-16.2 device/xiaomi/earth
 
+# Edited source 
+rm -rf packages/apps/Launcher3
+git clone https://github.com/AbuRider/packages_apps_Launcher3.git -b 16.2 packages/apps/Launcher3
+
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=kitauji_quartet
 
