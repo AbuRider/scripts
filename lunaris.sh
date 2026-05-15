2275bf2786d1e170af3aed9b6dcb68215dade38c
Makinohara <aburider@proton.me>
2026-05-15 17:18:30 +0700

    Update lunaris.sh

--- lunaris.sh
+++ lunaris.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-repo init -u https://github.com/The-Clover-Project/manifest.git -b 16-qpr2 --git-lfs
+rm -rf prebuilts/clang/host/linux-x86
+
+repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs
 /opt/crave/resync.sh || repo sync
 
-git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Clover-16.2 device/xiaomi/earth
+git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Lunaris-16.2 device/xiaomi/earth
 
 # Edited source 
-rm -rf hardware/clover/interfaces
-git clone https://github.com/LineageOS/android_hardware_lineage_interfaces.git -b lineage-23.2 hardware/lineage/interfaces
-git clone https://github.com/LineageOS/android_packages_apps_EuiccPolicy.git -b lineage-23.2 packages/apps/EuiccPolicy
+rm -rf packages/apps/Launcher3
+git clone https://github.com/AbuRider/packages_apps_Launcher3.git -b 16.2 packages/apps/Launcher3
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=kitauji_quartet
 
 . build/envsetup.sh
-lunch clover_earth-bp4a-userdebug
-mka clover
+lunch lineage_earth-bp4a-userdebug
+mka bacon
