c23cf52bf86d4e507f46b93f6be9adec27870833
Makinohara <aburider@proton.me>
2026-05-16 10:16:16 +0700

    clean up

--- pos.sh
+++ pos.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-rm -rf device/xiaomi/earth
+rm -rf prebuilts/clang/host/linux-x86
+
+repo init -u https://github.com/Kitauji-High-School/pixelos_manifest.git -b sixteen-qpr2 --git-lfs
+/opt/crave/resync.sh || repo sync
+
 git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b PixelOS-16.2 device/xiaomi/earth
 
 export BUILD_USERNAME=kumiko
@@ -7,5 +11,4 @@ export BUILD_HOSTNAME=kitauji_quartet
 
 . build/envsetup.sh
 breakfast earth userdebug
-make installclean
 m pixelos
