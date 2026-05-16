2dfadd8ec4d8819a01ad4967a53d818e3f188321
Makinohara <aburider@proton.me>
2026-05-16 14:11:48 +0700

    Update kkkk.sh

--- kkkk.sh
+++ kkkk.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 rm -rf prebuilts/clang/host/linux-x86
-rm -rf device/xiaomi/earth vendor/lineage-priv/keys
 
-repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs
+repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle
 /opt/crave/resync.sh || repo sync
 
-git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b Lunaris-16.2 device/xiaomi/earth
-git clone https://github.com/AbuRider/sign.git -b keys2 vendor/lineage-priv/keys
+git clone https://github.com/AbuRider/android_device_xiaomi_earth.git -b crDroid-16.2 device/xiaomi/earth
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=wind_orchestra
 
 . build/envsetup.sh
 lunch lineage_earth-bp4a-userdebug
-make installclean
-m bacon
+mka bacon
