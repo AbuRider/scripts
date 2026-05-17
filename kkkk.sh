99492c8cc193860392624ce56de8458455493ffc
Makinohara <aburider@proton.me>
2026-05-17 19:32:21 +0700

    Update kkkk.sh

--- kkkk.sh
+++ kkkk.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
-repo init -u https://github.com/Kitauji-High-School/matrixx_manifest -b 15.0 --git-lfs
+repo init -u https://github.com/ProjectMatrixx/android -b 16.2 --git-lfs
 /opt/crave/resync.sh || repo sync
 
-git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Matrixx-15 device/xiaomi/earth
+git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Matrixx-16.2 device/xiaomi/earth
 
 export BUILD_USERNAME=kumiko
-export BUILD_HOSTNAME=wind_orchestra
+export BUILD_HOSTNAME=kitauji_quartet
 
 . build/envsetup.sh
-brunch earth userdebug
+lunch matrixx_earth-bp4a-userdebug
+make matrixx
