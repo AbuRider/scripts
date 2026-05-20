fae6b6db2654b2b7e81c3bf97ed55a504e788467
Makinohara <aburider@proton.me>
2026-05-20 20:12:10 +0700

    Update kkkk.sh

--- kkkk.sh
+++ kkkk.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-
-repo init -u https://github.com/Kitauji-High-School/matrixx_manifest -b 15.0 --git-lfs
-/opt/crave/resync.sh || repo sync
-
-# git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Matrixx-15 device/xiaomi/earth
+rm -rf device/xiaomi/earth
+git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b crDroid-16.2 device/xiaomi/earth
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=kitauji_quartet
 
 . build/envsetup.sh
-lunch lineage_earth-bp1a-userdebug
+lunch lineage_earth-bp4a-userdebug
 make installclean
 mka bacon
