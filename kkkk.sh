add30920b495dc5bd8d8a6d1c7f85b488570ed43
Makinohara <aburider@proton.me>
2026-05-25 09:53:39 +0700

    Update kkkk.sh

--- kkkk.sh
+++ kkkk.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 rm -rf prebuilts/clang/host/linux-x86
 
-repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs
+repo init -u https://github.com/GenesisOS/manifest.git -b yume --git-lfs
 /opt/crave/resync.sh # sync source 
 
-git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Lunaris-16.2 device/xiaomi/earth
-
-rm -rf packages/apps/Launcher3
-git clone https://github.com/AbuRider/packages_apps_Launcher3.git -b 16.2 packages/apps/Launcher3
+git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Genesis-16.2 device/xiaomi/earth
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=kitauji_quartet
 
 . build/envsetup.sh
-lunch lineage_earth-bp4a-userdebug
-m bacon
+lunch genesis_earth-bp4a-userdebug
+mka genesis
 
 # Upload files to gofile
 echo "Upload to gofile will be started..."
