d92a22cc2a285811f819df20272a7b0d4e47c3f9
Makinohara <aburider@proton.me>
2026-05-26 04:15:46 +0700

    Update kkkk.sh

--- kkkk.sh
+++ kkkk.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-rm -rf prebuilts/clang/host/linux-x86
+# rm -rf prebuilts/clang/host/linux-x86
 
-repo init -u https://github.com/GenesisOS/manifest.git -b yume --git-lfs
-/opt/crave/resync.sh # sync source 
+# repo init -u https://github.com/GenesisOS/manifest.git -b yume --git-lfs
+# /opt/crave/resync.sh # sync source 
 
+rm -rf device/xiaomi/earth
 git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Genesis-16.2 device/xiaomi/earth
 
 # Edited source
-rm -rf hardware/genesis/interfaces
-git clone https://github.com/LineageOS/android_hardware_lineage_interfaces.git -b lineage-23.2 hardware/lineage/interfaces
-git clone https://github.com/LineageOS/android_packages_apps_EuiccPolicy.git -b lineage-23.2 packages/apps/EuiccPolicy
+rm -rf hardware/lineage/interfaces
+git clone https://github.com/AbuRider/android_hardware_lineage_interfaces.git -b lineage-23.2 hardware/genesis/interfaces
+# git clone https://github.com/LineageOS/android_packages_apps_EuiccPolicy.git -b lineage-23.2 packages/apps/EuiccPolicy
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=kitauji_quartet
 
 . build/envsetup.sh
 lunch genesis_earth-bp4a-userdebug
+make installclean
 mka genesis
 
 # Upload files to gofile
