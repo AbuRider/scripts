ef25ae64a54ff25139f5b347597deacdd66558be
Makinohara <aburider@proton.me>
2026-05-30 13:16:18 +0700

    Update kkkk.sh

--- kkkk.sh
+++ kkkk.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
-# rm -rf prebuilts/clang/host/linux-x86
+rm -rf prebuilts/clang/host/linux-x86
 
-# repo init -u https://github.com/GenesisOS/manifest.git -b yume --git-lfs
-# /opt/crave/resync.sh # sync source 
+repo init -u https://github.com/AndroidOne-Experience/manifest.git -b 15 --git-lfs
+/opt/crave/resync.sh # sync source 
 
-rm -rf device/xiaomi/earth
-git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Genesis-16.2 device/xiaomi/earth
-
-# Edited source
-rm -rf hardware/lineage/interfaces
-git clone https://github.com/AbuRider/android_hardware_lineage_interfaces.git -b lineage-23.2 hardware/genesis/interfaces
-# git clone https://github.com/LineageOS/android_packages_apps_EuiccPolicy.git -b lineage-23.2 packages/apps/EuiccPolicy
+git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b AO-15 device/xiaomi/earth
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=kitauji_quartet
 
 . build/envsetup.sh
-lunch genesis_earth-bp4a-userdebug
-make installclean
-mka genesis
+lunch aosp_earth-bp1a-userdebug
+mka bacon
 
 # Upload files to gofile
 echo "Upload to gofile will be started..."
