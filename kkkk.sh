49880ed134d2a176320b55465a84895e5b11b41b
Makinohara <aburider@proton.me>
2026-06-04 15:30:46 +0700

    Update kkkk.sh

--- kkkk.sh
+++ kkkk.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-# rm -rf prebuilts/clang/host/linux-x86
+rm -rf prebuilts/clang/host/linux-x86
 
-# repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.2 --git-lfs
-# /opt/crave/resync.sh # sync source 
+repo init -u https://github.com/Evolution-X/manifest -b bq2 --git-lfs
+/opt/crave/resync.sh # sync source 
 
-rm -rf hardware/mediatek
-git clone https://github.com/Kitauji-High-School/android_hardware_mediatek.git -b lineage-23.2 hardware/mediatek
+git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b EvolutionX-16.2 device/xiaomi/earth
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=kitauji_quartet
 
 . build/envsetup.sh
-axion earth userdebug va
-ax -br
+lunch lineage_earth-bp4a-userdebug
+m evolution
 
 # Upload files to gofile
 echo "Upload to gofile will be started..."
-if [ -f out/target/product/earth/*202606*.zip ]; then
+if [ -f out/target/product/earth/*11.8*.zip ]; then
     wget https://raw.githubusercontent.com/lordgaruda/GoFile-Upload/refs/heads/master/upload.sh
     chmod +x upload.sh ; ./upload.sh out/target/product/earth/*.zip
 fi
