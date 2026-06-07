c2198bc80320d9cb1626d12e3708e43d4ec568dc
Makinohara <aburider@proton.me>
2026-06-07 10:21:22 +0700

    Update kkkk.sh

--- kkkk.sh
+++ kkkk.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
-# rm -rf prebuilts/clang/host/linux-x86
+rm -rf prebuilts/clang/host/linux-x86
 
-repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs
+repo init -u https://github.com/Kitauji-High-School/pixelos_manifest.git -b sixteen-qpr2 --git-lfs
 /opt/crave/resync.sh # sync source 
 
-git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Lunaris-16.2 device/xiaomi/earth
-
-# Edited source
-rm -rf packages/apps/Launcher3
-git clone https://github.com/AbuRider/packages_apps_Launcher3.git -b 16.2 packages/apps/Launcher3
+git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b PixelOS-16.2 device/xiaomi/earth
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=kitauji_quartet
 
 . build/envsetup.sh
-lunch lineage_earth-bp4a-userdebug
-mka bacon
+breakfast earth userdebug
+m pixelos
 
 # Upload files to gofile
 echo "Upload to gofile will be started..."
