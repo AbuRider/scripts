dd441e05660f64e2fc0dfee08f33366127247e6a
Makinohara <aburider@proton.me>
2026-06-10 19:48:07 +0700

    Update kkkk.sh

--- kkkk.sh
+++ kkkk.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
 # init source
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 -u https://github.com/ProjectMatrixx/android -b 16.2 --git-lfs
 /opt/crave/resync.sh # sync source 
 
-git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Infinity-16.2 device/xiaomi/earth
+git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Matrixx-16.2 device/xiaomi/earth
+
+# Custom source 
+rm -rf packages/apps/Launcher3
+git clone https://github.com/AbuRider/packages_apps_Launcher3-Matrixx.git -b 16.2 packages/apps/Launcher3
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=kitauji_quartet
 
 . build/envsetup.sh
-lunch infinity_earth-userdebug
-m bacon
+lunch matrixx_earth-bp4a-userdebug
+mka matrixx
 
 # Upload files to gofile
 echo "Upload to gofile will be started..."
-if [ -f out/target/product/earth/*3.11*.zip ]; then
+if [ -f out/target/product/earth/*12.4*.zip ]; then
   wget https://raw.githubusercontent.com/lordgaruda/GoFile-Upload/refs/heads/master/upload.sh
   chmod +x upload.sh ; ./upload.sh out/target/product/earth/*.zip
 fi
-echo "hame"
+echo "kkkkk"
