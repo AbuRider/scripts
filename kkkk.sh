f67f36c36be82c05f043f5e8862db5e826e9fd9a
Makinohara <aburider@proton.me>
2026-06-15 14:26:47 +0700

    Update kkkk.sh

--- kkkk.sh
+++ kkkk.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 
-# init source
-repo init --depth=1 -u https://github.com/LumineDroid/platform_manifest.git -b bellflower --git-lfs
-/opt/crave/resync.sh # sync source 
-
-git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Lumine-16.2 device/xiaomi/earth
-git clone https://github.com/Kitauji-High-School/vendor_xiaomi_earth.git -b 16.2 vendor/xiaomi/earth
-git clone --depth=1 https://github.com/Kitauji-High-School/android_kernel_xiaomi_earth.git -b 16.2 kernel/xiaomi/earth
-git clone https://github.com/AbuRider/sign.git -b lumine vendor/lumine-priv/keys
-
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.2 hardware/mediatek
-git clone https://github.com/Kitauji-High-School/android_device_mediatek_sepolicy_vndr.git -b lumine device/mediatek/sepolicy_vndr
-git clone https://github.com/MillenniumOSS/android_vendor_mediatek_ims.git -b sixteen-qpr2 vendor/mediatek/ims
+git clone https://github.com/LineageOS/android_packages_apps_FMRadio.git -b lineage-23.2 packages/apps/FMRadio
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=kitauji_quartet
 
 . build/envsetup.sh
 lunch earth-bp4a-userdebug
+make installclean
 mka bacon
 
 # Upload files to gofile
