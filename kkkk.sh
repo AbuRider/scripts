a0c1796b5b051782410d1b9fd4b7f1be2845fd6d
Makinohara <aburider@proton.me>
2026-06-26 22:31:54 +0700

    Update kkkk.sh

--- kkkk.sh
+++ kkkk.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-rm -rf device/xiaomi/earth vendor/xiaomi/earth kernel/xiaomi/earth vendor/mediatek/ims
-rm -rf hardware/mediatek hardware/xiaomi device/mediatek/sepolicy_vndr vendor/evolution-priv/keys
 
 # init source
-repo init --depth=1 -u https://github.com/LumineDroid/platform_manifest -b camellia --git-lfs
-/opt/crave/resync.sh # crave repo sync
+# repo init --depth=1 -u https://github.com/LumineDroid/platform_manifest -b camellia --git-lfs
+# /opt/crave/resync.sh # crave repo sync
 
-git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Lumine-17.0 device/xiaomi/earth
-git clone https://github.com/LineageOS/android_packages_apps_FMRadio.git packages/apps/FMRadio
+git clone https://github.com/Evolution-X/vendor_qcom_opensource_wfd-commonsys.git -b cnb vendor/qcom/opensource/commonsys/wfd
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=kitauji_quartet
