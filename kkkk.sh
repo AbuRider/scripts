9c81a44f1f5aa4699c1b36fef3e29ff1868904df
Makinohara <aburider@proton.me>
2026-06-26 18:39:59 +0700

    Update kkkk.sh

--- kkkk.sh
+++ kkkk.sh
@@ -6,7 +6,7 @@ rm -rf hardware/mediatek hardware/xiaomi device/mediatek/sepolicy_vndr vendor/ev
 repo init --depth=1 -u https://github.com/LumineDroid/platform_manifest -b camellia --git-lfs
 /opt/crave/resync.sh # crave repo sync
 
-git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b lumine-17.0 device/xiaomi/earth
+git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Lumine-17.0 device/xiaomi/earth
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=kitauji_quartet
