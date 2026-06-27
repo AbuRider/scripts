c2b0c43f35a017c7c20dfc7465f2c13941ad6a5b
Makinohara <aburider@proton.me>
2026-06-27 16:40:50 +0700

    Update hi.sh

--- hi.sh
+++ hi.sh
@@ -4,8 +4,8 @@
 # repo init --depth=1 -u https://github.com/Evolution-X/manifest -b cnb --git-lfs
 # /opt/crave/resync.sh # crave repo sync
 
-rm -rf hardware/mediatek
-git clone https://github.com/Kitauji-High-School/android_hardware_mediatek.git -b lineage-24.0 hardware/mediatek
+# rm -rf hardware/mediatek
+# git clone https://github.com/Kitauji-High-School/android_hardware_mediatek.git -b lineage-24.0 hardware/mediatek
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=kitauji_quartet
@@ -13,7 +13,6 @@ export BUILD_HOSTNAME=kitauji_quartet
 # build start
 . build/envsetup.sh
 lunch lineage_earth-cp2a-userdebug
-make installclean
 m evolution
 
 # Upload files to gofile
