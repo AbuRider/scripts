3a62ff728fa60044619dd0c5b8a14e8e3d5cbae8
Makinohara <aburider@proton.me>
2026-06-28 07:05:15 +0700

    Update kkkk.sh

--- kkkk.sh
+++ kkkk.sh
@@ -6,6 +6,8 @@ rm -rf hardware/mediatek
 # repo init --depth=1 -u https://github.com/LumineDroid/platform_manifest -b camellia --git-lfs
 # /opt/crave/resync.sh # crave repo sync
 
+git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Lumine-17 device/xiaomi/earth
+
 rm -rf vendor/qcom/opensource/commonsys/wfd
 git clone https://github.com/Evolution-X/vendor_qcom_opensource_wfd-commonsys.git -b cnb vendor/qcom/opensource/commonsys/wfd
 
