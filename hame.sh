5977bee70161b4a5aa9c2c197f23c6ec4bd14484
Makinohara <aburider@proton.me>
2026-07-08 12:24:03 +0700

    Update hame.sh

--- hame.sh
+++ hame.sh
@@ -2,25 +2,24 @@
 rm -rf device/xiaomi/earth
 rm -rf hardware/mediatek device/mediatek/sepolicy_vndr
 
-# init source
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
-/opt/crave/resync.sh # crave repo sync
+# init rom source 
+repo init --depth=1 -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs
+/opt/crave/resync.sh # sync source
 
-git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Infinity-16.2 device/xiaomi/earth
+git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Lunaris-16.2 device/xiaomi/earth
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=kitauji_quartet
 
 # build start
 . build/envsetup.sh
-lunch infinity_earth-userdebug
-make installclean
+lunch lineage_earth-bp4a-userdebug
 m bacon
 
 # Upload files to gofile
 echo "Upload to gofile will be started..."
-if [ -f out/target/product/earth/*3.12*.zip ]; then
+if [ -f out/target/product/earth/*202607*.zip ]; then
   wget https://raw.githubusercontent.com/lordgaruda/GoFile-Upload/refs/heads/master/upload.sh
-  chmod +x upload.sh ; ./upload.sh out/target/product/earth/*3.12*.zip
+  chmod +x upload.sh ; ./upload.sh out/target/product/earth/boot.img out/target/product/earth/*202607*.zip
 fi
 echo "kkkkk"
