3c22901b0567de4108a615bc8be055959bb3d818
Makinohara <aburider@proton.me>
2026-07-09 13:49:57 +0700

    Update kkkk.sh

--- kkkk.sh
+++ kkkk.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-rm -rf device/xiaomi/earth
-rm -rf hardware/mediatek
 
 # init source
-repo init --depth=1 -u https://github.com/LumineDroid/platform_manifest -b camellia --git-lfs
+repo init --depth=1 -u https://github.com/LumineDroid/platform_manifest -b bellflower --git-lfs
 /opt/crave/resync.sh # crave repo sync
 
-git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Lumine-17 device/xiaomi/earth
-
-rm -rf vendor/qcom/opensource/commonsys/wfd
-git clone https://github.com/Evolution-X/vendor_qcom_opensource_wfd-commonsys.git -b cnb vendor/qcom/opensource/commonsys/wfd
+git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Lumine-16.2 device/xiaomi/earth
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=kitauji_quartet
 
 . build/envsetup.sh
-lunch earth-cp2a-userdebug
+lunch earth-bp4a-userdebug
 mka bacon
