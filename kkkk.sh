c4154b57632cbe6f799a98593cd22532993de47e
Makinohara <aburider@proton.me>
2026-07-14 07:14:54 +0700

    Update kkkk.sh

--- kkkk.sh
+++ kkkk.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # init source
-repo init --depth=1 -u https://github.com/AbuRider/luminedruid_manifest.git -b bellflower --git-lfs
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+# Clone local manifests
+git clone https://github.com/aosp-pablo/device_manifest.git -b infinity .repo/local_manifests
 /opt/crave/resync.sh # crave repo sync
 
-git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Lumine-16.2 device/xiaomi/earth
-
-export BUILD_USERNAME=kumiko
-export BUILD_HOSTNAME=kitauji_quartet
+export BUILD_USERNAME=Hhh
+export BUILD_HOSTNAME=Hhh
 
 . build/envsetup.sh
-lunch earth-bp4a-userdebug
+lunch infinity_marble-user
 mka bacon
