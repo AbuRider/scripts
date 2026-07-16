bbd44b4361048cb3bfd7d4ba59153354ec294a0d
Makinohara <aburider@proton.me>
2026-07-16 21:27:50 +0700

    Update luna.sh

--- luna.sh
+++ luna.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
+rm -rf device/xiaomi/earth vendor/xiaomi/earth kernel/xiaomi/earth
+rm -rf hardware/mediatek hardware/xiaomi device/mediatek/sepolicy_vndr
 
 # init rom source 
-# repo init --depth=1 -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs
-# /opt/crave/resync.sh # sync source
+repo init --depth=1 -u https://github.com/LineageOS/android.git -b lineage-24.0 --git-lfs
+/opt/crave/resync.sh # sync source
 
-rm -rf device/xiaomi/earth vendor/xiaomi/earth
-git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Lunaris-16.2 device/xiaomi/earth
+git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b lineage-24.0 device/xiaomi/earth
+git clone --depth=1 https://github.com/Kitauji-High-School/vendor_xiaomi_earth.git -b lineage-24.0 vendor/xiaomi/earth
+git clone --depth=1 https://github.com/Kitauji-High-School/android_kernel_xiaomi_earth.git -b staging kernel/xiaomi/earth
 
-# rm -rf vendor/lineage
-# git clone --depth=1 https://github.com/SilverEuphonium/vendor_lineage.git -b 16.2 vendor/lineage
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-24.0 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-24.0 hardware/mediatek
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-24.0 device/mediatek/sepolicy_vndr
+git clone https://github.com/MillenniumOSS/android_vendor_mediatek_ims.git -b sixteen-qpr2 vendor/mediatek/ims
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=kitauji_quartet
 
 # build start
 . build/envsetup.sh
-lunch lineage_earth-bp4a-userdebug
+lunch lineage_earth-cp2a-userdebug
 make installclean
 m bacon
 
