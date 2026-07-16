a250314c4f37222385b904145f8a8f565ee49025
Makinohara <aburider@proton.me>
2026-07-16 23:28:08 +0700

    Update luna.sh

--- luna.sh
+++ luna.sh
@@ -10,7 +10,7 @@ git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git
 git clone --depth=1 https://github.com/Kitauji-High-School/vendor_xiaomi_earth.git -b lineage-24.0 vendor/xiaomi/earth
 git clone --depth=1 https://github.com/Kitauji-High-School/android_kernel_xiaomi_earth.git -b staging kernel/xiaomi/earth
 
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-24.0 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-24.0 hardware/mediatek
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-24.0 device/mediatek/sepolicy_vndr
 git clone https://github.com/MillenniumOSS/android_vendor_mediatek_ims.git -b sixteen-qpr2 vendor/mediatek/ims
