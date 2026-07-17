515c28775a590234f19173a4096144e396122db8
Makinohara <aburider@proton.me>
2026-07-17 10:11:48 +0700

    Update kkkk.sh

--- kkkk.sh
+++ kkkk.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
+
+# Clean up old directory (Notes: Use if if necessary!)
 rm -rf .repo/local_manifests
+rm -rf device/xiaomi/earth vendor/xiaomi/earth kernel/xiaomi/earth
+rm -rf hardware/mediatek hardware/xiaomi device/mediatek/sepolicy_vndr
 
 # init rom source 
 repo init --depth=1 -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs
 /opt/crave/resync.sh # sync source
 
+# Device sources
 git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Lunaris-16.2 device/xiaomi/earth
 
+# Edited sources 
 rm -rf vendor/lineage
 git clone --depth=1 https://github.com/SilverEuphonium/vendor_lineage.git -b 16.2 vendor/lineage
 
