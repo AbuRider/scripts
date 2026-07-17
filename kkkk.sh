9c4cbc1ee19efa6c0b5a3a56043bbefe6c90ffe9
Makinohara <aburider@proton.me>
2026-07-17 14:35:04 +0700

    Update kkkk.sh

--- kkkk.sh
+++ kkkk.sh
@@ -2,19 +2,19 @@
 
 # Clean up old directory (Notes: Use it if necessary!)
 rm -rf .repo/local_manifests
-rm -rf device/xiaomi/earth vendor/xiaomi/earth kernel/xiaomi/earth
-rm -rf hardware/mediatek hardware/xiaomi device/mediatek/sepolicy_vndr
+rm -rf device/xiaomi/earth vendor/lineage-priv/keys # vendor/xiaomi/earth kernel/xiaomi/earth
+# rm -rf hardware/mediatek hardware/xiaomi device/mediatek/sepolicy_vndr
 
 # init rom source 
-repo init --depth=1 -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs
-/opt/crave/resync.sh # sync source
+# repo init --depth=1 -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs
+# /opt/crave/resync.sh # sync source
 
 # Device sources
 git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Lunaris-16.2 device/xiaomi/earth
 
 # Edited sources 
-rm -rf vendor/lineage
-git clone --depth=1 https://github.com/SilverEuphonium/vendor_lineage.git -b 16.2 vendor/lineage
+# rm -rf vendor/lineage
+# git clone --depth=1 https://github.com/SilverEuphonium/vendor_lineage.git -b 16.2 vendor/lineage
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=kitauji_quartet
@@ -22,6 +22,7 @@ export BUILD_HOSTNAME=kitauji_quartet
 # build start
 . build/envsetup.sh
 lunch lineage_earth-bp4a-userdebug
+make installclean 
 m bacon
 
 # Upload files to gofile
