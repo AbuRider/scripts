09c48c72f661bc1a92787c85f86c46c2a4529dc6
Makinohara <aburider@proton.me>
2026-07-19 12:51:41 +0700

    Update evok.sh

--- evok.sh
+++ evok.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Clean up directory
-rm -rf prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 # (Notes: it's has been depecrated on LineageOS 24.0, see: https://github.com/LineageOS/android/commit/de40b9789872d37f5bfd3ad703f801701c0e9ca6) 
+# rm -rf prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 # (Notes: it's has been depecrated on LineageOS 24.0, see: https://github.com/LineageOS/android/commit/de40b9789872d37f5bfd3ad703f801701c0e9ca6) 
 
 # init rom source 
-repo init --depth=1 -u https://github.com/Evolution-X/manifest.git -b cnb --git-lfs --no-clone-bundle
+repo init --depth=1 -u https://github.com/Evolution-X/manifest.git -b cnb --git-lfs
 /opt/crave/resync.sh # sync source
 
 # git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b EvolutionX-17.0 device/xiaomi/earth
@@ -19,9 +19,7 @@ make installclean
 m evolution
 
 # Upload files to gofile
-echo "Upload to gofile will be started..."
 if [ -f out/target/product/earth/*202607*.zip ]; then
   wget https://raw.githubusercontent.com/lordgaruda/GoFile-Upload/refs/heads/master/upload.sh
   chmod +x upload.sh ; ./upload.sh out/target/product/earth/boot.img ; ./upload.sh out/target/product/earth/*202607*.zip
 fi
-echo "kkkkk"
