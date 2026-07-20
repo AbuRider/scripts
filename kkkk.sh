a6496060ecf8194f6d777152a18e1d0e5dad5b17
Makinohara <aburider@proton.me>
2026-07-20 18:03:04 +0700

    Update kkkk.sh

--- kkkk.sh
+++ kkkk.sh
@@ -6,6 +6,14 @@ repo init --depth=1 -u https://github.com/LineageOS/android.git -b lineage-24.0 
 
 # Device sources
 git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b lineage-24.0 device/xiaomi/earth
+git clone https://github.com/Kitauji-High-School/vendor_xiaomi_earth.git -b lineage-24.0 vendor/xiaomi/earth --depth=1
+git clone https://github.com/Kitauji-High-School/android_kernel_xiaomi_earth.git -b staging kernel/xiaomi/earth --depth=1
+git clone https://github.com/AbuRider/sign.git -b keys3 vendor/lineage-priv/keys
+
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-24.0 hardware/mediatek
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-24.0 device/mediatek/sepolicy_vndr
+git clone https://github.com/MillenniumOSS/android_vendor_mediatek_ims.git -b sixteen-qpr2 vendor/mediatek/ims
 
 
 export BUILD_USERNAME=kumiko
