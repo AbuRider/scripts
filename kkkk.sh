0e414cb7810b2df0b3d5eaa218c1b02d8c63c709
Makinohara <aburider@proton.me>
2026-07-20 18:01:04 +0700

    Update kkkk.sh

--- kkkk.sh
+++ kkkk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # init rom source 
-repo init --depth=1 -u https://github.com/LineageOS/android -b lineage-24.0 --git-lfs
+repo init --depth=1 -u https://github.com/LineageOS/android.git -b lineage-24.0 --git-lfs
 /opt/crave/resync.sh # sync source
 
 # Device sources
