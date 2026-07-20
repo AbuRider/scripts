ab6fed7addfffbfe9a01a377598b87ca29b45d3a
Makinohara <aburider@proton.me>
2026-07-20 18:00:44 +0700

    Update kkkk.sh

--- kkkk.sh
+++ kkkk.sh
@@ -1,28 +1,19 @@
 #!/bin/bash
 
-# Clean up old directory (Notes: Use it if necessary!)
-rm -rf .repo/local_manifests
-rm -rf device/xiaomi/earth vendor/lineage-priv/keys vendor/xiaomi/earth kernel/xiaomi/earth
-rm -rf hardware/mediatek hardware/xiaomi device/mediatek/sepolicy_vndr vendor/mediatek/ims vendor/lineage
-
 # init rom source 
-repo init --depth=1 -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs
+repo init --depth=1 -u https://github.com/LineageOS/android -b lineage-24.0 --git-lfs
 /opt/crave/resync.sh # sync source
 
 # Device sources
-git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Lunaris-16.2 device/xiaomi/earth
+git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b lineage-24.0 device/xiaomi/earth
 
-# Edited sources 
-rm -rf vendor/lineage
-git clone --depth=1 https://github.com/SilverEuphonium/vendor_lineage.git -b 16.2 vendor/lineage
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=kitauji_quartet
 
 # build start
 . build/envsetup.sh
-lunch lineage_earth-bp4a-userdebug
-make installclean 
+lunch lineage_earth-cp2a-userdebug 
 m bacon
 
 # Upload files to gofile
