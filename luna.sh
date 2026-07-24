5eee8f698a6baa2743f0f52c034ce751425d3408
Makinohara <aburider@proton.me>
2026-07-24 16:03:12 +0700

    Update luna.sh

--- luna.sh
+++ luna.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
 # init rom source 
-repo init --depth=1 -u https://github.com/Kitauji-High-School/lunaris_manifests.git -b 16.2 --git-lfs
+repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs --depth=1
 /opt/crave/resync.sh # sync source
 
+# Device sources
 git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Lunaris-16.2 device/xiaomi/earth
+
+# Pathing Frameworks base
+cd frameworks/base
+curl -LSs "https://github.com/Kitauji-High-School/frameworks_base_lunaris/commit/6e3c8d137395ab002ae7060a6ead8678749d3430.patch" -o fwb.patch ; patch -p1 < fwb.patch
+cd ../..
+
+# Patching vendor/lineage
+cd vendor/lineage
+curl -LSs "https://github.com/Kitauji-High-School/vendor_lineage/commit/2a627f812f97556ef9222d9b3b3a3259db936303.patch" -o vendor.patch ; patch -p1 < vendor.patch
+cd ../..
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=kitauji_quartet
