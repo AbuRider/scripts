79e1618dd2a01a68afeeadf60d1e19317adee305
Makinohara <aburider@proton.me>
2026-08-01 23:19:27 +0700

    Update luna.sh

--- luna.sh
+++ luna.sh
@@ -5,11 +5,23 @@ rm -rf device/xiaomi/earth vendor/xiaomi/earth kernel/xiaomi/earth
 rm -rf hardware/xiaomi hardware/mediatek device/mediatek/sepolicy_vndr
 
 # init rom source 
-repo init -u https://github.com/Kitauji-High-School/lunaris_manifests.git -b 16.2 --git-lfs --depth=1
+repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs --depth=1
 /opt/crave/resync.sh # sync source
 
 # Device sources
 git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Lunaris-16.2 device/xiaomi/earth
+
+# Pathing Frameworks base
+cd frameworks/base
+wget https://raw.githubusercontent.com/AbuRider/scripts/refs/heads/main/fwb.patch ; patch -p1 < fwb.patch
+rm -rf fwb.patch
+cd ../..
+
+# Patching vendor/lineage
+cd vendor/lineage
+curl -LSs "https://github.com/Kitauji-High-School/vendor_lineage/commit/2a627f812f97556ef9222d9b3b3a3259db936303.patch" -o vendor.patch ; patch -p1 < vendor.patch
+rm -rf vendor.patch
+cd ../..
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=kitauji_quartet
@@ -21,9 +33,9 @@ make installclean
 m bacon
 
 # Upload files to gofile
-echo "Upload to gofile will be started..."
 if [ -f out/target/product/earth/*202607*.zip ]; then
-  wget https://raw.githubusercontent.com/lordgaruda/GoFile-Upload/refs/heads/master/upload.sh
-  chmod +x upload.sh ; ./upload.sh out/target/product/earth/boot.img ; ./upload.sh out/target/product/earth/*202607*.zip
+    echo "Upload to gofile will be started..."
+    wget https://raw.githubusercontent.com/lordgaruda/GoFile-Upload/refs/heads/master/upload.sh
+    chmod +x upload.sh ; ./upload.sh out/target/product/earth/boot.img ; ./upload.sh out/target/product/earth/*202608*.zip
+    echo "Upload Done!"
 fi
-echo "kkkkk"
