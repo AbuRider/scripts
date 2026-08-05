f797f67fac87689268a4256e5fbd5fe6e80ff4c5
Makinohara <aburider@proton.me>
2026-08-05 18:41:37 +0700

    Update luna.sh

--- luna.sh
+++ luna.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # init rom source 
-# repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs --depth=1
-# /opt/crave/resync.sh # sync source
+repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs --depth=1
+/opt/crave/resync.sh # sync source
 
 # Device sources
-# git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Lunaris-16.2 device/xiaomi/earth
+git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Lunaris-16.2 device/xiaomi/earth
 
 # Pathing Frameworks base
-# cd frameworks/base
-# wget https://raw.githubusercontent.com/AbuRider/scripts/refs/heads/main/fwb.patch ; patch -p1 < fwb.patch
-# rm -rf fwb.patch
-# cd ../..
+cd frameworks/base
+wget https://raw.githubusercontent.com/AbuRider/scripts/refs/heads/main/fwb.patch ; patch -p1 < fwb.patch
+rm -rf fwb.patch
+cd ../..
 
 # Patching vendor/lineage
-# cd vendor/lineage
-# curl -LSs "https://github.com/Kitauji-High-School/vendor_lineage/commit/2a627f812f97556ef9222d9b3b3a3259db936303.patch" -o vendor.patch ; patch -p1 < vendor.patch
-# rm -rf vendor.patch
-# cd ../..
+cd vendor/lineage
+curl -LSs "https://github.com/Kitauji-High-School/vendor_lineage/commit/2a627f812f97556ef9222d9b3b3a3259db936303.patch" -o vendor.patch ; patch -p1 < vendor.patch
+rm -rf vendor.patch
+cd ../..
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=kitauji_quartet
@@ -25,7 +25,6 @@ export BUILD_HOSTNAME=kitauji_quartet
 # build start
 . build/envsetup.sh
 lunch lineage_earth-bp4a-userdebug
-make installclean
 m bacon
 
 # Upload files to gofile
